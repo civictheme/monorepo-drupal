042b0217fe92c1194adf99a88efb279265906f3a
Anand <anand.toshniwal@qed42.com>
2021-09-15 14:29:42 +0530

    [CIVIC-216] Added tests and default content.

--- tests/behat/bootstrap/FeatureContext.php
+++ tests/behat/bootstrap/FeatureContext.php
@@ -11,16 +11,20 @@ use DrevOps\BehatSteps\TaxonomyTrait;
 use DrevOps\BehatSteps\WatchdogTrait;
 use DrevOps\BehatSteps\FieldTrait;
 use DrevOps\BehatSteps\PathTrait;
+use DrevOps\BehatSteps\JsTrait;
+use DrevOps\BehatSteps\WaitTrait;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends DrupalContext {
 
+  use JsTrait;
   use ContentTrait;
   use FieldTrait;
   use PathTrait;
   use TaxonomyTrait;
   use WatchdogTrait;
+  use WaitTrait;
 
 }
