10c41f62341f2ddac9fa91e0eae0b7a818304cf3
Anand Toshniwal <anand.toshniwal1993@gmail.com>
2021-09-15 20:15:29 +0530

    [CIVIC-215]  Accordion BE. (#13)

--- tests/behat/bootstrap/FeatureContext.php
+++ tests/behat/bootstrap/FeatureContext.php
@@ -5,15 +5,15 @@
  * Civic Demo Drupal context for Behat testing.
  */
 
-use DrevOps\BehatSteps\JsTrait;
-use DrevOps\BehatSteps\WaitTrait;
 use Drupal\DrupalExtension\Context\DrupalContext;
 use DrevOps\BehatSteps\ContentTrait;
 use DrevOps\BehatSteps\TaxonomyTrait;
 use DrevOps\BehatSteps\WatchdogTrait;
 use DrevOps\BehatSteps\FieldTrait;
 use DrevOps\BehatSteps\PathTrait;
+use DrevOps\BehatSteps\JsTrait;
 use DrevOps\BehatSteps\SelectTrait;
+use DrevOps\BehatSteps\WaitTrait;
 
 /**
  * Defines application features from the specific context.
