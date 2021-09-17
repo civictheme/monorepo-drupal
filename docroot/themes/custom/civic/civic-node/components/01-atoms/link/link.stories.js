1c9a7690d8e120a9e00f5ae35ec5991c42ac63ae
Alex Skrypnyk <alex@integratedexperts.com>
2021-09-17 16:05:33 +1000

    Updated variables.

--- docroot/themes/custom/civic/civic-node/components/01-atoms/link/link.stories.js
+++ docroot/themes/custom/civic/civic-node/components/01-atoms/link/link.stories.js
@@ -1,13 +1,13 @@
 import {text, boolean} from '@storybook/addon-knobs'
 
-import CTLink from './link.twig'
+import CivicLink from './link.twig'
 import './link.scss'
 
 export default {
   title: 'Atom/Link',
 }
 
-export const Link = () => CTLink({
+export const Link = () => CivicLink({
   text: text('Text', 'Link Text'),
   href: text('URL', '/'),
   new_window: boolean('Open in a new window', false),
