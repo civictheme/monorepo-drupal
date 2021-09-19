3992ff2878e3c8989ea3b66c473308949c723558
Alex Skrypnyk <alex@integratedexperts.com>
2021-09-19 16:29:54 +1000

    Update docroot/themes/custom/civic/civic-node/components/01-atoms/link-button/link-button.stories.js
    Co-authored-by: richardgaunt <57734756+richardgaunt@users.noreply.github.com>

--- docroot/themes/custom/civic/civic-node/components/01-atoms/link-button/link-button.stories.js
+++ docroot/themes/custom/civic/civic-node/components/01-atoms/link-button/link-button.stories.js
@@ -1,7 +1,7 @@
 import {text, radios, boolean} from '@storybook/addon-knobs'
 
 import CivicLinkButton from './link-button.twig'
-import '..//button/button.scss'
+import '../button/button.scss'
 
 export default {
   title: 'Atom/Link Button',
