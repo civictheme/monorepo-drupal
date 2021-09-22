0bc49bcd6b4f5d9f0bb06f0cb1bbc02d87e8f3ef
Anand <anand.toshniwal@qed42.com>
2021-09-23 01:58:45 +0530

    [CIVIC-65] Added Navigation card.

--- docroot/themes/custom/civic/civic-library/components/01-atoms/icon/icon.stories.js
+++ docroot/themes/custom/civic/civic-library/components/01-atoms/icon/icon.stories.js
@@ -1,24 +1,8 @@
 import { text, select } from '@storybook/addon-knobs'
+import { spritesheets, icons } from './icon-helper'
 
 import CivicIcon from './icon.twig'
 import './icon.scss'
-
-// Use the icons availabe in the assets directory to compile a list of spritesheets and icon IDs.
-const spritesheets = new Set()
-const icons = {}
-require.context('../../../assets/icons/', true, /\.svg$/).keys().forEach(path => {
-  // Get a list of all spritesheets.
-  const spritesheetName = path.substring(2, path.indexOf('/', 2)).replace(/\s/g, '-').toLowerCase()
-  const spritesheetURL = `/icons/civic-${spritesheetName}.svg`
-  spritesheets.add(spritesheetURL)
-
-  // Get all icons available within the spritesheets.
-  const iconName = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.')).toLowerCase().replace(/\s/g, '-').replace(/[^a-z0-9\-]+/, '')
-  if (!icons[spritesheetURL]) {
-    icons[spritesheetURL] = []
-  }
-  icons[spritesheetURL].push(`${spritesheetName}-${iconName}`)
-})
 
 export default {
   title: 'Atom/Icon',
---
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/navigation-card/navigation-card.stories.js
@@ -0,0 +1,57 @@
+import { text, boolean, radios, select, object } from '@storybook/addon-knobs'
+import { spritesheets, icons } from '../../01-atoms/icon/icon-helper'
+
+import CivicNavigationCard from './navigation-card.twig'
+import './navigation-card.scss'
+
+export default {
+  title: 'Molecule/Cards'
+}
+
+
+export const NavigationCard = () => {
+
+  // Knob tab names.
+  const navCard = 'Navigation card';
+  const iconList = 'Icon (Applies to card with icon.)';
+
+  const imageData = {
+    'src': 'https://via.placeholder.com/580x580',
+    'width': 580,
+    'height': 580,
+    'alt': 'image alt text'
+  };
+  
+  // Current component parameters.  
+  const navCardParams = {
+    theme: radios('Theme', {
+      'Light': 'light',
+      'Dark': 'dark'
+    }, 'light', navCard),
+    modifier_class: [radios('Type', {
+      'With image': 'civic-nav-card--large',
+      'Without image': 'civic-nav-card--small',
+      'With Icon': 'civic-nav-card--icon'
+    }, 'civic-nav-card--large', navCard)].join(' '),
+    title: text('Title', 'Navigation card heading which runs across two or three lines', navCard),
+    summary: text('Summary', 'Recommend keeping card summary short over two or three lines.', navCard),
+    image: object('Image  (Applies to card with image.)', imageData, navCard),
+    url: text('Card URL', 'https://google.com', navCard)
+  };
+
+  // Knob tabs order is decided on the basis of their order in story.
+  // Icon component parameters. 
+  const sheets = Array.from(spritesheets)
+  let spritesheet = select('Icon Pack', sheets, sheets[0], iconList)
+  let symbol = select('Symbol', icons[spritesheet], icons[spritesheet][0], iconList)
+  const colors = CIVIC_VARIABLES['civic-default-colors']
+
+  const IconParams = { 
+    spritesheet,
+    symbol,
+    icon_color: select('Color', colors, 'primary', iconList)
+  }
+
+  return CivicNavigationCard({ ...navCardParams, ...IconParams });
+}
+
