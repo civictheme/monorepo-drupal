05955ce326c013a34b30ea41cbd1a00f8d1cb419
Chris Darke <chrisdarke42@gmail.com>
2021-09-22 18:09:00 +0100

    [CIVIC-84] Updated Callout story

--- docroot/themes/custom/civic/civic-library/components/02-molecules/callout/callout.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/callout/callout.stories.js
@@ -1,4 +1,4 @@
-import {text, select, boolean, radios} from '@storybook/addon-knobs'
+import {text, radios} from '@storybook/addon-knobs'
 import CivicCallout from "./callout.twig";
 
 export default {
@@ -8,32 +8,21 @@ export default {
 let exampleSummary = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'
 
 export const Callout = () => CivicCallout({
-  modifier_class: [
-    radios(
-      'Type',
-      {
-        'Light': 'civic-callout--light',
-        'Dark': 'civic-callout--dark',
-      },
-      'civic-callout--light',
-    ),
-  ],
-  title: text('Title', 'Callout title from knob'),
-  summary: text('Summary', exampleSummary),
-  cta_text: text('CTA Text', 'CTA Button Text'),
-  cta_type: radios(
-    'CTA Type',
+  theme: radios(
+    'Theme',
     {
-      'Primary': 'civic-button--primary',
-      'Primary Accent': 'civic-button--primary-accent',
-      'Secondary': 'civic-button--secondary',
-      'Secondary Accent': 'civic-button--secondary-accent',
+      'Light': 'light',
+      'Dark': 'dark',
     },
-    'civic-button--primary',
+    'light',
+    'Callout'
   ),
-  cta_url: text('CTA URL', ''),
-  link_text: text('Link Text', 'Link Text from knob'),
-  link_url: text('Link URL', '')
+  title: text('Title', 'Callout title from knob', 'Callout'),
+  summary: text('Summary', exampleSummary, 'Callout'),
+  links: [
+    {text: text('Text', 'Primary CTA', 'Primary CTA'), url: text('URL', '', 'Primary CTA'), type: 'civic-button--primary'},
+    {text: text('Text', 'Secondary CTA', 'Secondary CTA'), url: text('URL', '', 'Secondary CTA'), type: 'civic-button--secondary'}
+  ]
 });
 
 
