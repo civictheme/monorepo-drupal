9859d5c06aa4e49c21abb53c4eefcfe48ef04de1
Anand <anand.toshniwal@qed42.com>
2021-09-23 07:50:28 +0530

    [CIVIC-65] Removed helper.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/navigation-card/navigation-card.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/navigation-card/navigation-card.stories.js
@@ -1,8 +1,27 @@
 import { text, boolean, radios, select, object } from '@storybook/addon-knobs'
-import { spritesheets, icons } from '../../01-atoms/icon/icon-helper'
 
 import CivicNavigationCard from './navigation-card.twig'
 import './navigation-card.scss'
+
+
+// @todo Find a way to make this reusable.
+const spritesheets = new Set()
+const icons = {}
+// Use the icons availabe in the assets directory to compile a list of spritesheets and icon IDs.
+require.context('../../../assets/icons/', true, /\.svg$/).keys().forEach(path => {
+  // Get a list of all spritesheets.
+  const spritesheetName = path.substring(2, path.indexOf('/', 2)).replace(/\s/g, '-').toLowerCase()
+  const spritesheetURL = `/icons/civic-${spritesheetName}.svg`
+  spritesheets.add(spritesheetURL)
+
+  // Get all icons available within the spritesheets.
+  const iconName = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.')).toLowerCase().replace(/\s/g, '-').replace(/[^a-z0-9\-]+/, '')
+  if (!icons[spritesheetURL]) {
+    icons[spritesheetURL] = []
+  }
+  icons[spritesheetURL].push(`${spritesheetName}-${iconName}`)
+});
+
 
 export default {
   title: 'Molecule/Cards'
@@ -39,8 +58,8 @@ export const NavigationCard = () => {
     url: text('Card URL', 'https://google.com', navCard)
   };
 
-  // Knob tabs order is decided on the basis of their order in story.
-  // Icon component parameters. 
+  //Knob tabs order is decided on the basis of their order in story.
+  //Icon component parameters. 
   const sheets = Array.from(spritesheets)
   let spritesheet = select('Icon Pack', sheets, sheets[0], iconList)
   let symbol = select('Symbol', icons[spritesheet], icons[spritesheet][0], iconList)
