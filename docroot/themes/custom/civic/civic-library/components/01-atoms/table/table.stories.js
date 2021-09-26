19c3dbd6769fe732d67d4148c64ee4593fcfcc26
Govind Maloo <govind@salsadigital.com.au>
2021-09-26 08:14:31 +0530

    [CIVIC-77] Added twig Dynamic raws and header template. (#65)
    * [CIVIC-77] Table component.
    
    * [CIVIC-77] Twig template dynmic raws and header.

--- docroot/themes/custom/civic/civic-library/components/01-atoms/table/table.stories.js
+++ docroot/themes/custom/civic/civic-library/components/01-atoms/table/table.stories.js
@@ -5,10 +5,17 @@ import CivicTable from './table.twig';
  * Storybook Definition.
  */
 export default {
-  title: 'Atom/Table'
+  title: 'Atom/Table',
+  component: CivicTable,
 };
 
-export const Table = () => CivicTable({
+export const Table = (args) => CivicTable({
+  ...args,
+  props: [
+    'Header',
+    'Rows',
+    'Striped',
+  ],
   theme: radios(
     'Theme',
     {
@@ -17,7 +24,33 @@ export const Table = () => CivicTable({
     },
     'light',
   ),
+  heading: text('Heading', 'This is table heading'),
   striped: boolean('Striped', true),
-  header: text('Header', 'This is table header.'),
   caption: text('Caption', 'Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Proin eget tortor risus.'),
 });
+
+// Table controls.
+Table.args = {
+  header: [
+    'Header 1',
+    'Header 2',
+    'Header 3',
+  ],
+  raws: [
+    [
+      'Row 1',
+      'Information about something',
+      'And this is important',
+    ],
+    [
+      'Row 2',
+      'Information about something',
+      'And this is important',
+    ],
+    [
+      'Row 3',
+      'Information about something',
+      'And this is important',
+    ],
+  ]
+}
