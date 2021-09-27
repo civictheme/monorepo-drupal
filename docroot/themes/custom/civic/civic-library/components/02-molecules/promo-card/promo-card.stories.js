13e3fa280074b818b544d68be5d43e103d2c0085
Chris Darke <chrisdarke42@gmail.com>
2021-09-27 18:45:15 +0100

    [CIVIC-60] Promo elements theming updates

--- docroot/themes/custom/civic/civic-library/components/02-molecules/promo-card/promo-card.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/promo-card/promo-card.stories.js
@@ -3,33 +3,45 @@ import CivicPromoCard from "./promo-card.twig";
 import imageFile from "../../../assets/image.png";
 
 export default {
-  title: 'Molecule/Promo Card'
+  title: 'Molecule/Cards'
 }
 
 let exampleSummary = 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.'
 
-const imageAttr = {
-  src: imageFile,
-  alt: 'Civic image',
-};
+export const PromoCard = () => {
+  const imageAttr = {
+    src: imageFile,
+    alt: 'Civic image',
+  };
 
-export const PromoCard = () => CivicPromoCard({
-  theme: radios(
-    'Theme',
-    {
-      'Light': 'light',
-      'Dark': 'dark',
+  const promoCardParams = {
+    theme: radios(
+      'Theme',
+      {
+        'Light': 'light',
+        'Dark': 'dark',
+      },
+      'light'
+    ),
+    title: text('Title', 'Promo Card title from knob'),
+    summary: text('Summary', exampleSummary),
+    url: text('Link URL', null),
+    image: {
+      src: text('Image path', imageAttr.src),
+      alt: text('Image alt text', imageAttr.alt)
     },
-    'light'
-  ),
-  title: text('Title', 'Promo Card title from knob'),
-  summary: text('Summary', exampleSummary),
-  url: text('Link URL', null),
-  image: {
-    src: text('Image path', imageAttr.src),
-    alt: text('Image alt text', imageAttr.alt)
-  },
-  content_top_display: boolean('Display Content Top', false),
-  date: text('Date', '1 Jun 1970'),
-  topic: text('Topic', 'Topic name')
-});
+    date: text('Date', '1 Jun 1970'),
+    topic: text('Topic', 'Topic name')
+  }
+
+  // Date format/
+  let options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  };
+
+  promoCardParams.date = new Date(promoCardParams.date).toLocaleDateString("en-uk", options);
+
+  return CivicPromoCard(promoCardParams);
+}
