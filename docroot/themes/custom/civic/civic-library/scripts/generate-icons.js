51990184c06aa2ac4821ef2a7ba33a782f283906
Alan <alan@salsadigital.com.au>
2021-09-30 18:01:20 +1000

    [CIVIC-250] Path extraction - now supports all attrs (not just d="") * Added example scripts to civic_demo * Added example icon override to civic_demo

--- docroot/themes/custom/civic/civic-library/scripts/generate-icons.js
+++ docroot/themes/custom/civic/civic-library/scripts/generate-icons.js
@@ -7,10 +7,14 @@ function getMatches (reg, text, matchIndex) {
   const returnVars = []
   let match = reg.exec(text)
   while (match !== null) {
-    returnVars.push(match[matchIndex])
+    returnVars.push(matchIndex ? match[matchIndex] : match)
     match = reg.exec(text)
   }
   return returnVars
+}
+
+function renderPath (path) {
+  return Object.keys(path).map(key => `"${key}": "${path[key]}"`).join(', ')
 }
 
 // Extract the name, width and path out of them.
@@ -18,16 +22,26 @@ const twigVariables = []
 iconUtils.getIconPaths().forEach(path => {
   const iconSvg = fs.readFileSync(path, 'utf-8')
 
-  const rPaths = new RegExp(' d="([A-Z0-9\\.\\s]+)"', 'gi')
   const rWidth = new RegExp('width="([0-9]+)"', 'gi')
   const rHeight = new RegExp('height="([0-9]+)"', 'gi')
+  const rPath = new RegExp('<path ([^\\/]+?)\\/>', 'gi')
+  const rAttr = new RegExp('([a-zA-Z\\-]+)="(.+?)"', 'gi')
 
-  const paths = getMatches(rPaths, iconSvg, 1)
+  const paths = getMatches(rPath, iconSvg, 1)
+  let pathArr = []
+  paths.forEach(pathHTML => {
+    const pathProps = {}
+    getMatches(rAttr, pathHTML, null).forEach(match => {
+      pathProps[match[1]] = match[2]
+    })
+    pathArr.push(pathProps)
+  })
+
+  const renderedPath = `[ ${pathArr.map(path => `{${renderPath(path)}}`)} ]`
   const width = getMatches(rWidth, iconSvg, 1)
   const height = getMatches(rHeight, iconSvg, 1)
   const name = iconUtils.getNameFromPath(path)
 
-  const renderedPath = `[${paths.map(item => `"${item}"`).join(',')}]`
   const twigVar = `"${name}": { "width": ${width[0]}, "height": ${height[0]}, "paths": ${renderedPath} }`
   twigVariables.push(twigVar)
 })
