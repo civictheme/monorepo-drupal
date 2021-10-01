61ae76f7b71e9fcd6262e6af548790e983c0901a
Alex Skrypnyk <alex@integratedexperts.com>
2021-10-01 16:56:46 +1000

    Added icon size.

--- docroot/themes/custom/civic/civic-library/components/01-atoms/icon/icon.stories.js
+++ docroot/themes/custom/civic/civic-library/components/01-atoms/icon/icon.stories.js
@@ -1,4 +1,4 @@
-import { select } from '@storybook/addon-knobs'
+import { radios, select } from '@storybook/addon-knobs'
 
 import CivicIcon from './icon.twig'
 import './icon.scss'
@@ -13,19 +13,21 @@ export default {
 export const Icon = () => {
   const icons = CIVIC_ICON.icons
   const colors = CIVIC_VARIABLES['civic-default-colors']
+  const sizes = CIVIC_VARIABLES['civic-icon-sizes']
 
   return CivicIcon({
     symbol: select('Symbol', icons, icons[0]),
     color: select('Color', colors, 'primary'),
+    size: radios('Size', sizes, sizes[0]),
   })
 }
 
 export const IconLibrary = () => {
   const packs = CIVIC_ICON.packs
   const colors = CIVIC_VARIABLES['civic-default-colors']
+  const sizes = CIVIC_VARIABLES['civic-icon-sizes']
 
   const selectedPack = select('Pack', Object.keys(packs), Object.keys(packs).length ? Object.keys(packs)[0] : null)
-  const selectedColor = select('Color', colors, 'primary')
 
   let html = ``
 
@@ -34,7 +36,8 @@ export const IconLibrary = () => {
     packs[selectedPack].forEach(icon => {
       html += CivicIcon({
         symbol: icon,
-        color: selectedColor
+        color: select('Color', colors, 'primary'),
+        size: radios('Size', sizes, sizes[0]),
       })
     })
 
