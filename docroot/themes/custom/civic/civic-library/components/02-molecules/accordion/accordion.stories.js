1efae0e35a271925542635de7bc0dc5775b15318
Richard Gaunt <richard.gaunt@protonmail.com>
2021-10-02 23:57:23 +1000

    Converted accordion story to knobs.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/accordion/accordion.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/accordion/accordion.stories.js
@@ -1,39 +1,53 @@
-import CivicAccordion from './accordion.twig'
-import './accordion.scss'
-import './accordion.js'
+import {
+  boolean, radios, text, number,
+} from '@storybook/addon-knobs';
+import CivicAccordion from './accordion.twig';
+// eslint-disable-next-line import/extensions
+import './accordion.js';
 
 export default {
   title: 'Molecule/Accordion',
-  component: CivicAccordion,
-  argTypes: {
-    theme: {
-      name: "Theme",
-      options: ['light', 'dark'],
-      mappings: {
-        'Light': 'light',
-        'Dark': 'dark',
-      },
-      defaultValue: 'light',
-      control: { type: 'radio' } // Automatically inferred when 'options' is defined
-    },
+  parameters: {
+    layout: 'centered',
   },
-}
+};
 
-export const Accordion = (args) => CivicAccordion({
-  ...args,
-})
-Accordion.args = {
-  panels: [
-    {
-      "title": "Acccordion item 1",
-      "content": "Accordion content",
-      "expanded": false,
-    },
-    {
-      "title": "Acccordion item 2",
-      "content": "Accordion content",
-      "expanded": true,
-    },
-  ],
-  expand_all: false,
-}
+export const Accordion = () => {
+  const generalKnobTab = 'General';
+  const expandAll = boolean('Expand all', false, generalKnobTab);
+  const generalKnobs = {
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      generalKnobTab,
+    ),
+    expand_all: expandAll,
+  };
+
+  // Adding dynamic number of accordion panels.
+  const panelsKnobTab = 'Panels';
+  const numOfPanels = number('Number of panels', 3, {}, panelsKnobTab);
+  const panels = [];
+  let itr = 1;
+  while (itr <= numOfPanels) {
+    panels.push({
+      title: text(`Panel ${itr} title `, `Acccordion title ${itr}`, panelsKnobTab),
+      content: text(`Panel ${itr} content`, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur harum magnam modi obcaecati vitae voluptatibus! Accusamus atque deleniti, distinctio esse facere, nam odio officiis omnis porro quibusdam quis repudiandae veritatis.', panelsKnobTab),
+      expanded: boolean(`Panel ${itr} expanded`, expandAll, panelsKnobTab),
+    });
+    itr += 1;
+  }
+
+  const panelKnobs = {
+    panels,
+  };
+
+  return CivicAccordion({
+    ...generalKnobs,
+    ...panelKnobs,
+  });
+};
