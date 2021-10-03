0415ebcde47f80ed1865c8087852c1eb4f6bed6f
Richard Gaunt <richard.gaunt@protonmail.com>
2021-10-03 12:20:31 +1100

    WIP - card container story refactor.

--- docroot/themes/custom/civic/civic-library/components/03-organisms/card-container/card-container.stories.js
+++ docroot/themes/custom/civic/civic-library/components/03-organisms/card-container/card-container.stories.js
@@ -1,9 +1,20 @@
 import {
-  boolean, radios, text, select,
+  boolean, radios, number, text, select,
 } from '@storybook/addon-knobs';
 import CivicCardContainer from './card-container.twig';
-import imageFile from '../../../assets/image.png';
+import { PromoCard } from '../../02-molecules/promo-card/promo-card.stories';
+import { EventCard } from '../../02-molecules/event-card/event-card.stories';
+import { NavigationCard } from '../../02-molecules/navigation-card/navigation-card.stories';
+import { ServiceCard } from '../../02-molecules/service-card/service-card.stories';
+
 import './card-container.scss';
+
+const CardMap = {
+  'promo-card': PromoCard,
+  'event-card': EventCard,
+  'navigation-card': NavigationCard,
+  'service-card': ServiceCard,
+};
 
 export default {
   title: 'Organism/Card Container',
@@ -11,19 +22,7 @@ export default {
 
 export const CardContainer = () => {
   const generalKnobTab = 'General';
-  const cardsKnobTab = 'Cards';
 
-  const typeOfCard = radios(
-    'Type of card',
-    {
-      Promo: 'promo-card',
-      Event: 'event-card',
-      Navigation: 'navigation-card',
-      Service: 'service-card',
-    },
-    'promo-card',
-    generalKnobTab,
-  );
   const generalKnobs = {
     theme: radios(
       'Theme',
@@ -42,13 +41,26 @@ export const CardContainer = () => {
     modifier_class: text('Additional class', '', generalKnobTab),
   };
 
-  generalKnobs.date = new Date(generalKnobs.date).toLocaleDateString('en-uk', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
+  const typeOfCard = radios(
+    'Type of card',
+    {
+      Promo: 'promo-card',
+      Event: 'event-card',
+      Navigation: 'navigation-card',
+      Service: 'service-card',
+    },
+    'promo-card',
+    generalKnobTab,
+  );
+
+  const numOfCards = number('Number of cards', 4, generalKnobTab);
+  const cards = [];
+  for (let itr = 0; itr < numOfCards; itr += 1) {
+    cards.push(CardMap[typeOfCard]);
+  }
 
   return CivicCardContainer({
     ...generalKnobs,
+    cards,
   });
 };
