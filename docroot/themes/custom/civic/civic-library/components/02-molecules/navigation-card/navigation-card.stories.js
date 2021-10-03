975ddaba4d989aa5baf6fee7c76d86f82b25ecf4
Alex Skrypnyk <alex@integratedexperts.com>
2021-10-04 09:24:53 +1100

    Fixed code lint issues.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/navigation-card/navigation-card.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/navigation-card/navigation-card.stories.js
@@ -1,16 +1,18 @@
-import { boolean, radios, select, text } from '@storybook/addon-knobs'
+import {
+  boolean, radios, select, text,
+} from '@storybook/addon-knobs';
 import imageFile from '../../../assets/image.png';
 
-import CivicNavigationCard from './navigation-card.twig'
-import './navigation-card.scss'
-import { getSlots } from "../../00-base/base.stories";
+import CivicNavigationCard from './navigation-card.twig';
+import './navigation-card.scss';
+import { getSlots } from '../../00-base/base.stories';
 
 export default {
   title: 'Molecule/Navigation Card',
   parameters: {
     layout: 'centered',
   },
-}
+};
 
 export const NavigationCard = () => {
   const generalKnobTab = 'General';
@@ -18,20 +20,20 @@ export const NavigationCard = () => {
     theme: radios(
       'Theme',
       {
-        'Light': 'light',
-        'Dark': 'dark'
+        Light: 'light',
+        Dark: 'dark',
       },
       'light',
-      generalKnobTab
+      generalKnobTab,
     ),
     size: radios(
       'Size',
       {
-        'Large': 'large',
-        'Small': 'small'
+        Large: 'large',
+        Small: 'small',
       },
       'large',
-      generalKnobTab
+      generalKnobTab,
     ),
     title: text('Title', 'Navigation card heading which runs across two or three lines', generalKnobTab),
     summary: text('Summary', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.', generalKnobTab),
@@ -58,6 +60,6 @@ export const NavigationCard = () => {
       'content_top',
       'content_middle',
       'content_bottom',
-    ])
+    ]),
   });
-}
+};
