7b112c270f4c069d171cf498a637892d1e6d93ea
Alex Skrypnyk <alex@integratedexperts.com>
2021-10-05 21:51:12 +1100

    Refactored header notifications into alerts.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/message/message.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/message/message.stories.js
@@ -5,7 +5,7 @@ export default {
   title: 'Molecule/Message',
 };
 
-export const Messages = () => CivicMessage({
+export const Message = () => CivicMessage({
   theme: radios(
     'Theme',
     {
@@ -27,4 +27,5 @@ export const Messages = () => CivicMessage({
   ),
   title: text('Title', 'The information on this page is currently being updated.'),
   description: text('Summary', 'Filium morte multavit si sine causa, nollem me tamen laudandis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel elit laoreet, dignissim arcu sit amet, vulputate risus.'),
+  modifier_class: text('Additional class', ''),
 });
