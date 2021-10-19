4b21c9764726cde25f88338aba7243ce339d36b7
Alex Skrypnyk <alex@integratedexperts.com>
2021-10-19 21:36:08 +1100

    Updated markup in link list.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/link-list/link-list.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/link-list/link-list.stories.js
@@ -1,5 +1,5 @@
 import {
-  radios, text, object,
+  boolean, object, radios, text,
 } from '@storybook/addon-knobs';
 
 import CivicLinkList from './link-list.twig';
@@ -12,33 +12,23 @@ export default {
   },
 };
 
-export const LinkList = () => {
-  const links = [
+export const LinkList = () => CivicLinkList({
+  theme: radios(
+    'Theme',
+    {
+      Light: 'light',
+      Dark: 'dark',
+    },
+    'light',
+  ),
+  title: text('Title', 'Optional list title'),
+  is_inline: boolean('Inline', false),
+  modifier_class: text('Additional class', ''),
+  links: object('Links', [
     { title: 'Link title 1', url: '#' },
     { title: 'Link title 2', url: '#' },
-    { title: 'Link title 3 long title goes over multiple line', url: '#' },
+    { title: 'Link title 3 long title goes over multiple lines', url: '#' },
     { title: 'Link title 4', url: '#', new_window: true },
     { title: 'Link title 5', url: 'http://google.com', is_external: true },
-  ];
-
-  return CivicLinkList({
-    theme: radios(
-      'Theme',
-      {
-        Light: 'light',
-        Dark: 'dark',
-      },
-      'light',
-    ),
-    type: radios(
-      'Type',
-      {
-        Default: 'default',
-        Inline: 'inline',
-      },
-      'default',
-    ),
-    modifier_class: text('Additional class', ''),
-    links: object('Links', links),
-  });
-};
+  ]),
+});
