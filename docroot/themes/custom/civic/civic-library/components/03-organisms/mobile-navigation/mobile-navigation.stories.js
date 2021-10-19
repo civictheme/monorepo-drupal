14ad9ff6f81559735c38ad855adc36955d2fc0b3
Nick Georgiou <nickgeorgi@gmail.com>
2021-10-19 18:16:22 +1000

    Mobile navigation

---
+++ docroot/themes/custom/civic/civic-library/components/03-organisms/mobile-navigation/mobile-navigation.stories.js
@@ -0,0 +1,115 @@
+import {
+  radios,
+  object,
+} from '@storybook/addon-knobs';
+import CivicMobileNavigation from './mobile-navigation.twig';
+import './mobile-navigation.scss';
+
+export default {
+  title: 'Organisms/Mobile Navigation',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const MobileNavigation = () => {
+  const links = [
+    {
+      title: 'About us',
+      url: '#',
+      below: [
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Help',
+      url: '#',
+      below: [
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+        {
+          title: 'Menu item',
+          url: '#',
+        },
+      ],
+    },
+    {
+      title: 'Services',
+      url: '#',
+      below: [
+        {
+          title: 'External link',
+          url: '#',
+          new_window: true,
+          is_external: true,
+        },
+        {
+          title: 'External link',
+          url: '#',
+          new_window: true,
+          is_external: true,
+        },
+        {
+          title: 'External link',
+          url: '#',
+          new_window: true,
+          is_external: true,
+        },
+      ],
+    },
+    {
+      title: 'Contact Us',
+      url: '#',
+    },
+  ];
+
+  const generalKnobTab = 'General';
+
+  const generalKnobs = {
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      generalKnobTab,
+    ),
+    items: object('Links', links, generalKnobTab),
+    icon_size: radios(
+      'Icon Size',
+      {
+        Small: 'small',
+        Regular: 'regular',
+      },
+      'regular',
+      generalKnobTab,
+    ),
+  };
+
+  return CivicMobileNavigation({
+    ...generalKnobs,
+    menu_type: 'main',
+  });
+};
