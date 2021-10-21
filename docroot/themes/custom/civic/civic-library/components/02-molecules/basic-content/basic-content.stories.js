4b6efb0e8821e66a00441a928ec42ac9908e39d9
Alan <alan@salsadigital.com.au>
2021-10-21 21:56:31 +1100

    [CIVIC-276] Basic Content - Add element support.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/basic-content/basic-content.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/basic-content/basic-content.stories.js
@@ -6,17 +6,93 @@ export default {
   title: 'Molecules/Basic Content',
   parameters: {
     layout: 'fullscreen',
+    knobs: {
+      escapeHTML: false,
+    },
   },
 };
 
-export const BasicContent = () => CivicBasicContent({
-  theme: radios(
-    'Theme',
-    {
+let html = ''
+
+// Headings
+html += `
+  <h1>Heading 1</h2>
+  <h2>Heading 2</h2>
+  <h3>Heading 3</h2>
+  <h4>Heading 4</h2>
+  <h5>Heading 5</h2>
+  <h6>Heading 6</h2>
+`;
+
+// Paragraphs
+html += `
+  <p class="lead">Sed aute in sed consequat veniam excepteur minim mollit.</p>
+  <p>Veniam reprehenderit velit ea veniam occaecat magna est sed duis quis elit occaecat dolore ut enim est do in dolor non elit aliquip commodo aliquip sint veniam ullamco adipisicing tempor ad.</p>
+  <p><span>Duis sunt velit.</span><span>Ea eu non.</span></p>
+  <p>In mollit in minim ut non <a class="civic-link" href="https://example.com">commodo dolore</a> nisi anim.</p>
+`;
+
+// Blockquote
+html += `
+  <blockquote cite="https://example.com">Culpa laboris sit fugiat minim ad commodo eu id sint eu sed nisi.</blockquote>
+`;
+
+// Lists
+html += `
+  <ul>
+    <li>Sint pariatur quis tempor.</li>
+    <li>Lorem ipsum dolore laborum nulla ut.</li>
+    <li>Deserunt ullamco occaecat anim cillum.</li>
+  </ul>
+  <ol>
+    <li>Id nostrud id sit nulla.</li>
+    <li>Dolore ea cillum culpa nulla.</li>
+    <li>Lorem ipsum ex excepteur.</li>
+  </ol>
+`;
+
+// Table
+html += `
+  <div class="civic-table civic-theme-light civic-table--striped">
+    <table>
+      <thead>
+        <tr>
+          <th>Column A</th>
+          <th>Column B</th>
+          <th>Column C</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Do duis minim cupidatat eu.</td>
+          <td>Ullamco sunt dolore.</td>
+          <td>Dolor in officia.</td>
+        </tr>
+        <tr>
+          <td>Lorem ipsum magna sint.</td>
+          <td>Consequat qui anim.</td>
+          <td>Lorem ipsum aliqua veniam deserunt.</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+`;
+
+export const BasicContent = () => {
+  const generalKnobTab = 'General';
+  const theme = radios(
+    'Theme', {
       Light: 'light',
       Dark: 'dark',
     },
     'light',
-  ),
-  content: text('Content', 'Filium morte multavit si sine causa, nollem me tamen laudandis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel elit laoreet, dignissim arcu sit amet, vulputate risus.'),
-});
+    generalKnobTab,
+  );
+
+  const generalKnobs = {
+    theme: theme,
+    content: html // TODO - Add this back when dev finished.--> text('Content', html, generalKnobTab),
+  };
+
+  return CivicBasicContent(generalKnobs);
+}
