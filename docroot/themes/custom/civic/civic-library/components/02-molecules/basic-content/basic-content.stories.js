2799688ff775b2597c9b6cb6de0ea72f6558aae4
Alan <alan@salsadigital.com.au>
2021-10-22 15:00:19 +1100

    [CIVIC-276] Basic Content - Clean up some styles. * Fix mobile table ::before title. Now uses a zero-based index.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/basic-content/basic-content.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/basic-content/basic-content.stories.js
@@ -1,6 +1,11 @@
 import { radios, text } from '@storybook/addon-knobs';
 
 import CivicBasicContent from './basic-content.twig';
+import CivicLink from '../../01-atoms/link/link.twig';
+import CivicTable from '../../01-atoms/table/table.twig';
+import CivicImage from '../../01-atoms/image/image.twig';
+import CivicVideo from '../../01-atoms/video/video.twig';
+import imageFile from '../../../assets/image.png';
 
 export default {
   title: 'Molecules/Basic Content',
@@ -12,72 +17,6 @@ export default {
   },
 };
 
-let html = ''
-
-// Headings
-html += `
-  <h1>Heading 1</h2>
-  <h2>Heading 2</h2>
-  <h3>Heading 3</h2>
-  <h4>Heading 4</h2>
-  <h5>Heading 5</h2>
-  <h6>Heading 6</h2>
-`;
-
-// Paragraphs
-html += `
-  <p class="lead">Sed aute in sed consequat veniam excepteur minim mollit.</p>
-  <p>Veniam reprehenderit velit ea veniam occaecat magna est sed duis quis elit occaecat dolore ut enim est do in dolor non elit aliquip commodo aliquip sint veniam ullamco adipisicing tempor ad.</p>
-  <p><span>Duis sunt velit.</span><span>Ea eu non.</span></p>
-  <p>In mollit in minim ut non <a class="civic-link" href="https://example.com">commodo dolore</a> nisi anim.</p>
-`;
-
-// Blockquote
-html += `
-  <blockquote cite="https://example.com">Culpa laboris sit fugiat minim ad commodo eu id sint eu sed nisi.</blockquote>
-`;
-
-// Lists
-html += `
-  <ul>
-    <li>Sint pariatur quis tempor.</li>
-    <li>Lorem ipsum dolore laborum nulla ut.</li>
-    <li>Deserunt ullamco occaecat anim cillum.</li>
-  </ul>
-  <ol>
-    <li>Id nostrud id sit nulla.</li>
-    <li>Dolore ea cillum culpa nulla.</li>
-    <li>Lorem ipsum ex excepteur.</li>
-  </ol>
-`;
-
-// Table
-html += `
-  <div class="civic-table civic-theme-light civic-table--striped">
-    <table>
-      <thead>
-        <tr>
-          <th>Column A</th>
-          <th>Column B</th>
-          <th>Column C</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Do duis minim cupidatat eu.</td>
-          <td>Ullamco sunt dolore.</td>
-          <td>Dolor in officia.</td>
-        </tr>
-        <tr>
-          <td>Lorem ipsum magna sint.</td>
-          <td>Consequat qui anim.</td>
-          <td>Lorem ipsum aliqua veniam deserunt.</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-`;
-
 export const BasicContent = () => {
   const generalKnobTab = 'General';
   const theme = radios(
@@ -88,6 +27,87 @@ export const BasicContent = () => {
     'light',
     generalKnobTab,
   );
+
+  let html = ''
+
+  // Headings
+  html += `
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+  `;
+
+  // Paragraphs
+  html += `
+    <p class="lead">Lead sed aute in sed consequat veniam excepteur minim mollit.</p>
+    <p>Body veniam reprehenderit velit ea veniam occaecat magna est sed duis quis elit occaecat dolore ut enim est do in dolor non elit aliquip commodo aliquip sint veniam ullamco adipisicing tempor ad.</p>
+    <p class="small">Small <span>duis sunt velit.</span><span>Ea eu non.</span></p>
+    <p>In mollit in minim ut non ${ CivicLink({ theme: theme, text: 'commodo dolore', url: 'https://example.com' }) } nisi anim.</p>
+    <p>Deserunt in ex dolore. <sup>Super cupidatat esse.</sup> <sub>Sub do mollit aute labore.</sub></p>
+  `;
+
+  // Blockquote
+  html += `
+    <blockquote cite="https://example.com">Culpa laboris sit fugiat minim ad commodo eu id sint eu sed nisi.</blockquote>
+  `;
+
+  // Lists
+  html += `
+    <ul>
+      <li>Sint pariatur quis tempor.</li>
+      <li>Lorem ipsum dolore laborum nulla ut.</li>
+      <li>Deserunt ullamco occaecat anim cillum.</li>
+    </ul>
+    <ol>
+      <li>Id nostrud id sit nulla.</li>
+      <li>Dolore ea cillum culpa nulla.</li>
+      <li>Lorem ipsum ex excepteur.</li>
+    </ol>
+  `;
+
+  // Image
+  html += CivicImage({
+    theme: theme,
+    src: imageFile,
+    alt: 'Occaecat laborum voluptate cupidatat.',
+    caption: 'Commodo anim sint minim.',
+  });
+
+  // Video
+  html += CivicVideo({
+    theme: theme,
+    src: 'https://www.youtube.com/embed/C0DPdy98e4c',
+  });
+
+  // Table
+  html += CivicTable({
+    theme: theme,
+    header: [
+      'Column A',
+      'Column B',
+      'Column C',
+    ],
+    rows: [
+      [
+        'Do duis minim cupidatat eu.',
+        'Ullamco sunt dolore.',
+        'Dolor in officia.',
+      ],
+      [
+        'Do duis minim cupidatat eu.',
+        'Ullamco sunt dolore.',
+        'Dolor in officia.',
+      ],
+      [
+        'Lorem ipsum magna sint.',
+        'Consequat qui anim.',
+        'Lorem ipsum aliqua veniam deserunt.',
+      ],
+    ]
+  });
 
   const generalKnobs = {
     theme: theme,
