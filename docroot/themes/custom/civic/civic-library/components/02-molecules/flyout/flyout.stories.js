9b512fe3058950ea3256667ea2e85787f470fa31
Nick Georgiou <nickgeorgi@gmail.com>
2021-10-22 13:42:30 +1000

    Add components and variables for mobile-navigation, mobile-navigation-item, flyout components.

---
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/flyout/flyout.stories.js
@@ -0,0 +1,45 @@
+import {
+  radios, text, boolean,
+} from '@storybook/addon-knobs';
+
+import CivicFlyout from './flyout.twig';
+import './flyout.scss';
+import './flyout';
+
+export default {
+  title: 'Molecule/Flyout',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Flyout = () => {
+  const content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur harum magnam modi obcaecati vitae voluptatibus! Accusamus atque deleniti, distinctio esse facere, nam odio officiis omnis porro quibusdam quis repudiandae veritatis.';
+
+  const html = CivicFlyout({
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+    ),
+    flyout_from: radios(
+      'Flyout from',
+      {
+        Right: 'right',
+        Left: 'left',
+        Top: 'top',
+        Bottom: 'bottom',
+      },
+      'right',
+    ),
+    trigger: text('Trigger', 'Open Flyout'),
+    content: text('Content', content),
+    modifier_class: text('Additional class', ''),
+    expanded: boolean('Expanded', false),
+  });
+
+  return `<div class="story-wrapper-size--medium">${html}</div>`;
+};
