5299ceaa9e9208385364f8a197f0a66634c4b5eb
Alan <alan@salsadigital.com.au>
2021-10-28 15:46:16 +1100

    [CIVIC-67] Add table of contents component.

---
+++ docroot/themes/custom/civic/civic-library/components/03-organisms/table-of-contents/table-of-contents.stories.js
@@ -0,0 +1,65 @@
+import {
+  radios, text, object,
+} from '@storybook/addon-knobs';
+import CivicTableOfContents from './table-of-contents.twig';
+
+export default {
+  title: 'Organisms/Table Of Contents',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const TableOfContents = (knobTab) => {
+  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
+
+  const generalKnobs = {
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      generalKnobTab,
+    ),
+    title: text('Title', 'On this page', generalKnobTab),
+    links: object('Links', [
+      {
+        text: 'Introduction',
+        url: '#introduction',
+        active: '',
+      },
+      {
+        text: 'Section 1',
+        url: '#section-1',
+        active: '',
+      },
+      {
+        text: 'Section 2',
+        url: '#section-2',
+        active: '',
+      },
+      {
+        text: 'Section 3',
+        url: '#section-3',
+        active: '',
+      },
+      {
+        text: 'Section 4',
+        url: '#section-4',
+        active: '',
+      },
+      {
+        text: 'Section 5',
+        url: '#section-5',
+        active: '',
+      },
+    ], generalKnobTab),
+    modifier_class: text('Additional class', '', generalKnobTab),
+  };
+
+  return CivicTableOfContents({
+    ...generalKnobs,
+  });
+};
