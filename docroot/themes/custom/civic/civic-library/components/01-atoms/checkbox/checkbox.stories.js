90dc16c2e4f57125f5e3086db34ea71a20af35c1
Richard Gaunt <richard.gaunt@protonmail.com>
2021-10-28 22:51:05 +1100

    Refactored checkbox.

--- docroot/themes/custom/civic/civic-library/components/01-atoms/checkbox/checkbox.stories.js
+++ docroot/themes/custom/civic/civic-library/components/01-atoms/checkbox/checkbox.stories.js
@@ -3,35 +3,45 @@ import { boolean, radios, text } from '@storybook/addon-knobs';
 import CivicCheckbox from './checkbox.twig';
 
 export default {
-  title: 'Atoms/Input',
+  title: 'Atoms/Form',
   parameters: {
     layout: 'centered',
   },
 };
 
-export const Checkbox = () => CivicCheckbox({
-  theme: radios(
-    'Theme',
-    {
-      Light: 'light',
-      Dark: 'dark',
-    },
-    'light',
-  ),
-  type: 'checkbox',
-  value: text('Value', 'Civic input'),
-  label: text('Label', 'Civic input label'),
-  state: radios(
-    'State',
-    {
-      None: 'none',
-      Error: 'error',
-      Success: 'success',
-    },
-    'none',
-  ),
-  disabled: boolean('Disabled', false),
-  required: boolean('Required', false),
-  modifier_class: text('Additional class', ''),
-  attributes: text('Additional attributes', ''),
-});
+export const Checkbox = (knobTab) => {
+  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
+
+  const generalKnobs = {
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      generalKnobTab,
+    ),
+    type: 'checkbox',
+    value: text('Value', 'Civic input', generalKnobTab),
+    label: text('Label', 'Civic input label', generalKnobTab),
+    state: radios(
+      'State',
+      {
+        None: 'none',
+        Error: 'error',
+        Success: 'success',
+      },
+      'none',
+      generalKnobTab,
+    ),
+    disabled: boolean('Disabled', false, generalKnobTab),
+    required: boolean('Required', false, generalKnobTab),
+    modifier_class: text('Additional class', '', generalKnobTab),
+    attributes: text('Additional attributes', '', generalKnobTab),
+  };
+
+  return CivicCheckbox({
+    ...generalKnobs,
+  });
+};
