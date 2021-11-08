0cf9511a13506bf904308adf5527353f117b5542
Anand Toshniwal <anand.toshniwal@qed42.com>
2021-11-08 06:39:06 +0530

    CIVIC-57: Fixed back to top as per the feedback.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/back-to-top/back-to-top.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/back-to-top/back-to-top.js
@@ -1,8 +1,34 @@
-// Set focous on top of page element.
-// @todo Check window scroll to show/hide back to top button.
-document.querySelector('[data-component-name="back-to-top"]').addEventListener('click', () => {
-  const topOfPage = document.getElementById('#civic-top-of-page');
-  if (topOfPage !== null) {
-    topOfPage.focus();
+const header = document.querySelector('[data-component-name="header"]');
+const backToTop = document.querySelector('[data-component-name="back-to-top"]');
+const topIdExists = document.getElementById('civic-top-of-page');
+let siteHeaderHeight = 148;
+
+// Helper function to show/hide back to top button on scroll.
+function scrollFunction(headerHeight) {
+  const scrollTop = window.pageYOffset
+    || document.documentElement.scrollTop
+    || document.body.scrollTop
+    || 0;
+
+  if (scrollTop > headerHeight || document.documentElement.scrollTop > headerHeight) {
+    if (backToTop.classList.contains('civic-back-to-top--fade-out')) {
+      backToTop.classList.remove('civic-back-to-top--fade-out');
+    }
+    backToTop.classList.add('civic-back-to-top--fade-in');
+  } else {
+    if (backToTop.classList.contains('civic-back-to-top--fade-in')) {
+      backToTop.classList.remove('civic-back-to-top--fade-in');
+    }
+    backToTop.classList.add('civic-back-to-top--fade-out');
   }
-}, false);
+}
+
+// Calculate header height.
+if (header !== null) {
+  siteHeaderHeight = header.offsetHeight;
+}
+
+// Check if top id exists in the page else keep back to top hidden.
+if (topIdExists !== null) {
+  window.onscroll = () => { scrollFunction(siteHeaderHeight); };
+}
