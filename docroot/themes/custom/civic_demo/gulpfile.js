1694cf8e1b9ac52671a72e00c297b8f1ac837a15
Richard Gaunt <richard.gaunt@protonmail.com>
2021-11-08 23:50:07 +1100

    Added civic namespacing to civic theme.

--- docroot/themes/custom/civic_demo/gulpfile.js
+++ docroot/themes/custom/civic_demo/gulpfile.js
@@ -1,59 +1,56 @@
-const { src, dest, series, watch } = require('gulp');
-const fs = require('fs')
+const {
+  src,
+  dest,
+  series,
+  watch,
+} = require('gulp');
+const fs = require('fs');
 const flatMap = require('gulp-flatmap');
-const rename = require('gulp-rename')
-const del = require('del')
+const rename = require('gulp-rename');
+const del = require('del');
 const newer = require('gulp-newer');
-const log = require('fancy-log')
 
 // Component file globs.
-const civicStorybookWatchDir = __dirname + '/../civic/components/**'
-const civicChildStorybookWatchDir = __dirname + '/components/**'
+const civicStorybookWatchDir = `${__dirname}/../civic/components/**`;
+const civicChildStorybookWatchDir = `${__dirname}/components/**`;
 
 // Theme names - `civic_demo` needs to be updated to civic child theme name.
 // @todo Extract child theme name dynamically.
-const baseThemeName = __dirname.replace('civic_demo', 'civic')
+const baseThemeName = __dirname.replace('civic_demo', 'civic');
 
-const childThemeName = __dirname
+const childThemeName = __dirname;
 
 // Output directory of merged components.
-const outputDir = __dirname + '/components-combined'
-const civicComponentsDir = __dirname + '/components-civic';
-
-// Periodic rebuild of combined storybook.
-let periodicCleanup = function cleanUp() {
-  log('Running Periodic rebuild of storybook')
-  cleanTask(() => {})
-  buildTask(() => {log('Completed Periodic rebuild.')})
-};
+const outputDir = `${__dirname}/components-combined`;
+const civicComponentsDir = `${__dirname}/.components-civic`;
 
 // Add files to combined storybook.
 function buildTask(cb) {
-  let filePath
+  let filePath;
   // Import civic components to child theme to allow extending.
   src(civicStorybookWatchDir)
     .pipe(dest(civicComponentsDir));
 
   src(civicStorybookWatchDir)
-    .pipe(flatMap((stream , file) => {
-       filePath = file.path
-       if(filePath !== undefined) {
-         filePath = filePath
-           .replace(__dirname, '')
-           .replace(baseThemeName, childThemeName)
-         if (fs.existsSync(filePath)) {
-           return src(filePath)
-             .pipe(rename(function (path) {
-               path.dirname = filePath
-                 .replace(__dirname + '/components', '')
-                 .replace(path.basename + path.extname, '')
+    .pipe(flatMap((stream, file) => {
+      filePath = file.path;
+      if (filePath !== undefined) {
+        filePath = filePath
+          .replace(__dirname, '')
+          .replace(baseThemeName, childThemeName)
+        if (fs.existsSync(filePath)) {
+          return src(filePath)
+            .pipe(rename((path) => {
+              path.dirname = filePath
+                .replace(`${__dirname}/components`, '')
+                .replace(path.basename + path.extname, '');
 
-               return path
-             }))
-         }
-       }
+              return path;
+            }));
+        }
+      }
 
-       return stream
+      return stream;
     }))
     .pipe(newer(outputDir))
     .pipe(dest(outputDir));
@@ -63,28 +60,23 @@ function buildTask(cb) {
     .pipe(newer(outputDir))
     .pipe(dest(outputDir));
 
-  cb()
+  cb();
 }
 
 // Rebuild storybook to clear out deleted items.
 async function cleanTask(cb) {
-  await del(outputDir + '/**')
-  cb()
-}
-
-// Sets a periodic rebuild fixes bugs with file watcher.
-async function periodicCleanTask(cb) {
-  setTimeout(periodicCleanup, 1000 * 120)
+  await del(`${outputDir}/**`);
+  cb();
 }
 
 // Watch files for changes.
 function watchTask(cb) {
   watch([civicStorybookWatchDir, civicChildStorybookWatchDir], {
     persistent: true,
-    ignoreInitial: true
+    ignoreInitial: true,
   }, series(buildTask));
 
-  cb()
+  cb();
 }
-exports.default = series(cleanTask, buildTask)
-exports.watch = series(watchTask)
+exports.default = series(cleanTask, buildTask);
+exports.watch = series(watchTask);
