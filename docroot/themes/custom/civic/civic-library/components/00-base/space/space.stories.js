237231fcaaf6760277487fe071b7f3d2a8f955ba
Alex Skrypnyk <alex@integratedexperts.com>
2021-11-10 20:19:37 +1100

    Updated spacing stories class prefix.

--- docroot/themes/custom/civic/civic-library/components/00-base/space/space.stories.js
+++ docroot/themes/custom/civic/civic-library/components/00-base/space/space.stories.js
@@ -3,16 +3,16 @@ export default {
 };
 
 export const Space = () => `<div class="story-space-wrapper">
-  <div class="space--10">
-    <div class="space--9">
-      <div class="space--8">
-        <div class="space--7">
-          <div class="space--6">
-            <div class="space--5">
-              <div class="space--4">
-                <div class="space--3">
-                  <div class="space--2">
-                    <div class="space--1"></div>
+  <div class="story-space--10">
+    <div class="story-space--9">
+      <div class="story-space--8">
+        <div class="story-space--7">
+          <div class="story-space--6">
+            <div class="story-space--5">
+              <div class="story-space--4">
+                <div class="story-space--3">
+                  <div class="story-space--2">
+                    <div class="story-space--1"></div>
                   </div>
                 </div>
               </div>
