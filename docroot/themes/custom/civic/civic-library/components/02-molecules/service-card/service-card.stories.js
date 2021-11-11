ab0ee287fb5f5679096d0fe1a53010d877dbe259
Alex Skrypnyk <alex@integratedexperts.com>
2021-11-11 15:40:28 +1100

    Udpated Service Card to use mixins.

--- docroot/themes/custom/civic/civic-library/components/02-molecules/service-card/service-card.stories.js
+++ docroot/themes/custom/civic/civic-library/components/02-molecules/service-card/service-card.stories.js
@@ -1,7 +1,5 @@
-import {
-  boolean, radios, text,
-} from '@storybook/addon-knobs';
-import { getSlots, randomUrl } from '../../00-base/base.stories';
+import { number, radios, text } from '@storybook/addon-knobs';
+import { getSlots, randomLinks } from '../../00-base/base.stories';
 
 import CivicServiceCard from './service-card.twig';
 
@@ -27,32 +25,17 @@ export const ServiceCard = (knobTab) => {
       generalKnobTab,
     ),
     title: text('Title', 'Services category title across one or two lines', generalKnobTab),
-    links: boolean('With links', true, generalKnobTab) ? [
+    links: randomLinks(number(
+      'Number of links',
+      5,
       {
-        url: randomUrl(),
-        text: 'Service link 1',
-        new_window: false,
-        is_external: false,
+        range: true,
+        min: 0,
+        max: 10,
+        step: 1,
       },
-      {
-        url: randomUrl(),
-        text: 'Service link 2',
-        new_window: false,
-        is_external: false,
-      },
-      {
-        url: randomUrl(),
-        text: 'Service link 3',
-        new_window: false,
-        is_external: false,
-      },
-      {
-        url: randomUrl(),
-        text: 'Service link 4',
-        new_window: false,
-        is_external: false,
-      },
-    ] : null,
+      generalKnobTab,
+    ), 10),
     modifier_class: text('Additional class', '', generalKnobTab),
   };
 
