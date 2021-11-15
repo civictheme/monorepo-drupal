0d3566ef89095176a95ce6ff822d7cc27d61e775
Alex Skrypnyk <alex@integratedexperts.com>
2021-11-15 20:20:32 +1100

    Fixed SCSS importer and updated font variables structure to support multiple sources.

--- docroot/themes/custom/civic/civic-library/.storybook/importer.scss_variables.js
+++ docroot/themes/custom/civic/civic-library/.storybook/importer.scss_variables.js
@@ -14,15 +14,75 @@ function removeComments(string) {
   return lines.join('\n');
 }
 
+function getSassVariablesFromValueMap(string, maxLevel) {
+  maxLevel = maxLevel || 1;
+
+  const tokens = [];
+  let braces = 0;
+  let quotes = 0;
+
+  string = string.trim();
+  if (string.substring(0, 1) === '(' && (string.substring(string.length - 1) === ')' || string.substring(string.length - 2) === ');')) {
+    string = string.substring(1);
+    string = string.substring(string.length - 1) === ')' ? string.substring(0, string.length - 1) : string.substring(0, string.length - 2);
+  }
+
+  // Validate structures.
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '(') {
+      braces++;
+    } else if (string[i] === ')') {
+      braces--;
+    } else if (string[i] === "'") {
+      quotes++;
+    }
+  }
+
+  if (braces !== 0) {
+    throw new Error('Missing paired parenthesis.');
+  }
+
+  if (quotes % 2 !== 0) {
+    throw new Error('Missing closing quotes.');
+  }
+
+  let token = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '(') {
+      braces++;
+    } else if (string[i] === ')') {
+      braces--;
+    } else if (string[i] === "'") {
+      quotes++;
+    } else if (quotes > 0) {
+      if (quotes % 2 === 1) {
+        if (braces < maxLevel) {
+          token += string[i];
+        }
+      } else if (braces < maxLevel) {
+        tokens.push(token);
+        token = '';
+        quotes = 0;
+      }
+    }
+  }
+
+  return tokens.filter((value) => value || false);
+}
+
 function getSassVariablesFromValue(value) {
-  const vars = [];
+  let vars = [];
 
-  const re = new RegExp('\'[a-z-_0-9]+\'', 'gi');
+  if (value.indexOf('(') !== -1) {
+    vars = getSassVariablesFromValueMap(value);
+  } else {
+    const re = new RegExp('\'[a-z-_0-9]+\'', 'gi');
 
-  let match = re.exec(value);
-  while (match !== null) {
-    vars.push(match[0].replace(/[':]/gi, ''));
-    match = re.exec(value);
+    let match = re.exec(value);
+    while (match !== null) {
+      vars.push(match[0].replace(/[':]/gi, ''));
+      match = re.exec(value);
+    }
   }
 
   return vars;
@@ -31,7 +91,7 @@ function getSassVariablesFromValue(value) {
 function getVariablesFromFile(file) {
   const allValues = {};
 
-  let importedFile = fs.readFileSync(file, {encoding: 'utf8'});
+  let importedFile = fs.readFileSync(file, { encoding: 'utf8' });
 
   importedFile = removeComments(importedFile);
 
@@ -43,9 +103,13 @@ function getVariablesFromFile(file) {
     const match = re.exec(value);
     if (match) {
       const name = match[0].replace(/[$:]/gi, '');
-      let valueStripped = value.replace(/\$[a-z-]+:/gi, '');
-      valueStripped = valueStripped.replace(/\(.+?\)/gi, '');
-      valueStripped = valueStripped.replace(/:\s\([\s\S\n\r]+?\)/gi, '');
+      let valueStripped = value;
+      valueStripped = valueStripped.trim();
+      // Remove name.
+      valueStripped = valueStripped.replace(/\$[a-z-]+:/gi, '');
+      // Remove new lines and double spaces.
+      valueStripped = valueStripped.replace(/[\n\r]/gi, '');
+      valueStripped = valueStripped.replace(/[\s]{2,}/gi, ' ');
       allValues[name] = getSassVariablesFromValue(valueStripped);
     }
   });
