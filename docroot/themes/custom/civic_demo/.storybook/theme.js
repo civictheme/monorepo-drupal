27677ac3396dcfd973b2e65e90f078191a3ff011
Alex Skrypnyk <alex@integratedexperts.com>
2021-11-17 20:06:29 +1100

    Fixed assets paths.

--- docroot/themes/custom/civic_demo/.storybook/theme.js
+++ docroot/themes/custom/civic_demo/.storybook/theme.js
@@ -3,11 +3,9 @@
  */
 import { create } from '@storybook/theming';
 
-import logoUrl from '../assets/logos/civic_demo_logo_desktop_light.png';
-
 export default create({
   base: 'light',
   brandTitle: 'Civic Demo',
   brandUrl: 'https://github.com/salsadigitalauorg/civic',
-  brandImage: logoUrl,
+  brandImage: './assets/logos/civic_demo_logo_desktop_light.png',
 });
