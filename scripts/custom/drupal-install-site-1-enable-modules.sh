671b934ac0e14e8c253426e2c6dfe21a7e919dbf
Alex Skrypnyk <alex@integratedexperts.com>
2021-12-01 06:54:13 +1100

    Added Civic Companion module.

--- scripts/custom/drupal-install-site-1-enable-modules.sh
+++ scripts/custom/drupal-install-site-1-enable-modules.sh
@@ -18,6 +18,9 @@ DRUSH_ALIAS="${DRUSH_ALIAS:-}"
 # Use local or global Drush, giving priority to a local drush.
 drush="$(if [ -f "${APP}/vendor/bin/drush" ]; then echo "${APP}/vendor/bin/drush"; else command -v drush; fi)"
 
+# Use civic_companion.install to remove GovCMS or vanilla Drupal entities.
+$drush ${DRUSH_ALIAS} pm-enable civic_companion -y
+
 # Use cd_core.info.yml to declare all dependencies that must be installed in all environments.
 # Use cd_core.install to add other post-install operations.
 # Use cd_core.post_update.php to content-related post-install operations.
