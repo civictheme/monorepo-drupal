1e652f64e1667575b377a06d85ed3a2a99ba0e50
Alan <alan@salsadigital.com.au>
2021-12-14 13:13:31 +1100

    [CIVIC-298] Filters - Revert webpack file for civic demo.

--- docroot/themes/custom/civic_demo/webpack/webpack.common.js
+++ docroot/themes/custom/civic_demo/webpack/webpack.common.js
@@ -24,9 +24,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '../dist/styles.css',
     }),
-    new MiniCssExtractPlugin({
-      filename: '../dist/ckeditor.css',
-    }),
     new CleanWebpackPlugin(),
   ],
   module: {
@@ -55,14 +52,7 @@ module.exports = {
             options: {
               // Inject path to assets so that it does not have to be provided
               // in variables.base.scss
-              // additionalData: "$civic-assets-directory: '/themes/custom/civic_demo/dist/assets/';",
-              additionalData: (content, loaderContext) => {
-                // More information about available properties https://webpack.js.org/api/loaders/.
-                const { resourcePath, rootContext } = loaderContext;
-                const relativePath = path.relative(rootContext, resourcePath);
-                console.log(loaderContext);
-                return "$civic-assets-directory: '/themes/custom/civic_demo/dist/assets/'; .rg-wrapper {" + content + '}';
-              },
+              additionalData: "$civic-assets-directory: '/themes/custom/civic_demo/dist/assets/';",
               sourceMap: true,
               sassOptions: {
                 importer: magicImporter(),
