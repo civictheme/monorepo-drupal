428dcb804ff99c45d26a105ac0af456e0a1da70b
Alex Skrypnyk <alex@integratedexperts.com>
2021-12-14 15:10:58 +1100

    Added a post-install script to create a sub-theme.

---
+++ scripts/custom/drupal-install-site-3-create-subtheme.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+##
+# Create a sub-theme.
+#
+# shellcheck disable=SC2086
+
+set -e
+[ -n "${DREVOPS_DEBUG}" ] && set -x
+
+# Path to the application.
+APP="${APP:-/app}"
+
+# Drush alias.
+DRUSH_ALIAS="${DRUSH_ALIAS:-}"
+
+# ------------------------------------------------------------------------------
+
+# Use local or global Drush, giving priority to a local drush.
+drush="$(if [ -f "${APP}/vendor/bin/drush" ]; then echo "${APP}/vendor/bin/drush"; else command -v drush; fi)"
+
+echo "==> Create civic_demo sub-theme."
+
+pushd docroot/themes/custom/civic >/dev/null || exit 1
+php civic-create-subtheme.php civic_demo "Civic Demo" "Demo sub-theme for a Civic theme."
+[ ! -d docroot/themes/custom/civic_demo ] && echo "ERROR: Failed to create civic_demo sub-theme." && exit 1
+popd >/dev/null || exit 1
+
+if command -v npm &> /dev/null; then
+  pushd docroot/themes/custom/civic_demo >/dev/null || exit 1
+  echo "  > Installing FE dependencies."
+  npm ci
+
+  echo "  > Running FE build."
+  npm run build
+  popd >/dev/null || exit 1
+fi
+
+echo "  > Install a civic_demo theme."
+$drush ${DRUSH_ALIAS} theme:enable civic_demo -y
+
+echo "  > Making civic_demo a default theme."
+$drush ${DRUSH_ALIAS} config-set system.theme default civic_demo
+
+echo "  > Updating civic_demo theme settings."
+$drush ${DRUSH_ALIAS} ev "module_load_include('inc', 'cd_core', 'cd_core.civic_demo'); cd_core_civic_demo_update_theme_settings();"
