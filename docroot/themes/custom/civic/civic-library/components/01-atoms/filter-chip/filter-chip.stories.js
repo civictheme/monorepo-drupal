c3e0ce28485fa8c94335ceecfa515cc25c1ff13f
Alan <alan@salsadigital.com.au>
2021-12-15 11:52:33 +1100

    [CIVIC-298] Reverting filter-chip changes.

--- docroot/themes/custom/civic/civic-library/components/01-atoms/filter-chip/filter-chip.stories.js
+++ docroot/themes/custom/civic/civic-library/components/01-atoms/filter-chip/filter-chip.stories.js
@@ -1,7 +1,6 @@
 import { boolean, radios, text } from '@storybook/addon-knobs';
 
 import CivicFilterChip from './filter-chip.twig';
-import CivicFilterChipButton from './filter-chip-button.twig';
 
 export default {
   title: 'Atoms/Filter Chip',
@@ -31,28 +30,6 @@ export const FilterChip = (knobTab) => {
   };
 
   return CivicFilterChip({
-    ...generalKnobs,
-  });
-};
-
-export const FilterChipButton = (knobTab) => {
-  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
-
-  const generalKnobs = {
-    theme: radios(
-      'Theme', {
-        Light: 'light',
-        Dark: 'dark',
-      },
-      'light',
-      generalKnobTab,
-    ),
-    text: text('Text', 'Filter Chip text', generalKnobTab),
-    modifier_class: text('Additional class', '', generalKnobTab),
-    is_selected: boolean('Is selected', false, generalKnobTab),
-  };
-
-  return CivicFilterChipButton({
     ...generalKnobs,
   });
 };
