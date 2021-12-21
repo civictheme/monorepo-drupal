ad52509c62a631a9dea9f04d0af4c6bc7e61205b
richardgaunt <57734756+richardgaunt@users.noreply.github.com>
2021-12-21 14:24:03 +1100

    Fix starter theme build process. (#290)
    * Add copy cmd for custom theme dir.
    
    * Added create submtheme to dockerfile.
    
    * Running sub-theme cmd in build, splitting out DB required cmds to own post-rollout script.
    
    * Updated script.

--- scripts/build-starter-theme.sh
+++ scripts/build-starter-theme.sh
@@ -21,29 +21,9 @@ drush="$(if [ -f "${APP}/vendor/bin/drush" ]; then echo "${APP}/vendor/bin/drush
 echo "==> Create civic_demo sub-theme."
 
 pushd $APP/docroot/themes/contrib/civic >/dev/null || exit 1
-
+pwd
+ls
 php civic-create-subtheme.php civic_demo "Civic Demo" "Demo sub-theme for a Civic theme."
 [ ! -d $APP/docroot/themes/custom/civic_demo ] && echo "ERROR: Failed to create civic_demo sub-theme." && exit 1
 
 popd >/dev/null || exit 1
-
-echo "  > Installing civic_demo theme."
-$drush ${DRUSH_ALIAS} theme:enable civic_demo -y
-
-echo "  > Making civic_demo a default theme."
-$drush ${DRUSH_ALIAS} config-set system.theme default civic_demo -y
-
-echo "  > Updating civic_demo theme settings."
-$drush ${DRUSH_ALIAS} ev "module_load_include('inc', 'cd_core', 'cd_core.civic_demo'); cd_core_civic_demo_update_theme_settings();"
-
-if [ "$SKIP_SUBTHEME_FE" != "1" ] && command -v npm &> /dev/null; then
-  pushd $APP/docroot/themes/custom/civic_demo >/dev/null || exit 1
-
-  echo "  > Installing FE dependencies."
-  npm ci
-
-  echo "  > Running FE build."
-  npm run build
-
-  popd >/dev/null || exit 1
-fi
