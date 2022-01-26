6872f0abe1a4c00fa31ca628c3165c667684df72
Amey Mudras <amey.mudras@salsadigital.com.au>
2022-01-26 18:01:36 +0530

    CIVIC-80: Added embed google maps.

---
+++ docroot/themes/contrib/civic/civic-library/components/01-atoms/map/map.stories.js
@@ -0,0 +1,35 @@
+import { radios, text } from '@storybook/addon-knobs';
+import { randomUrl } from '../../00-base/base.stories';
+import CivicMap from './map.twig';
+
+export default {
+  title: 'Atoms/Map',
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export const Map = (knobTab) => {
+  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
+
+  const generalKnobs = {
+    theme: radios(
+      'Theme',
+      {
+        Light: 'light',
+        Dark: 'dark',
+      },
+      'light',
+      generalKnobTab,
+    ),
+    embed_url: text('Embed URL', 'https://maps.google.com/maps?q=australia&t=&z=3&ie=UTF8&iwloc=&output=embed', generalKnobTab),
+    address: text('Address', 'Australia', generalKnobTab),
+    share_link: text('Share Link', randomUrl(), generalKnobTab),
+    view_link: text('View Link', randomUrl(), generalKnobTab),
+    zoom: text('Zoom', '', generalKnobTab),
+  };
+
+  return CivicMap({
+    ...generalKnobs,
+  });
+};
