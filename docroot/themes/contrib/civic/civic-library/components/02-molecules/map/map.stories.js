7d9cf468dae26571da861cd9a86649272bf2a387
Richard Gaunt <richard.gaunt@protonmail.com>
2022-01-29 15:05:38 +1100

    Updated map story to handle external links.

--- docroot/themes/contrib/civic/civic-library/components/02-molecules/map/map.stories.js
+++ docroot/themes/contrib/civic/civic-library/components/02-molecules/map/map.stories.js
@@ -1,4 +1,4 @@
-import { radios, text } from '@storybook/addon-knobs';
+import { boolean, radios, text } from '@storybook/addon-knobs';
 import { randomUrl } from '../../00-base/base.stories';
 import CivicMap from './map.twig';
 
@@ -25,7 +25,9 @@ export const Map = (knobTab) => {
     embed_url: text('Embed URL', 'https://maps.google.com/maps?q=australia&t=&z=3&ie=UTF8&iwloc=&output=embed', generalKnobTab),
     address: text('Address', 'Australia', generalKnobTab),
     share_link: text('Share Link', randomUrl(), generalKnobTab),
+    share_link_is_external: boolean('Is share link external', false, generalKnobTab),
     view_link: text('View Link', randomUrl(), generalKnobTab),
+    view_link_is_external: boolean('Is view link external', false, generalKnobTab),
   };
 
   return CivicMap({
