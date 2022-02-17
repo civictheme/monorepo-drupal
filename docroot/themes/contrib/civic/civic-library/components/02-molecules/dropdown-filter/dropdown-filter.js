560e0a9805ce0ceb9611d5308e1bb3b8edc05051
Nick Georgiou <nickgeorgi@gmail.com>
2022-02-17 11:37:16 +1000

    Comments.

--- docroot/themes/contrib/civic/civic-library/components/02-molecules/dropdown-filter/dropdown-filter.js
+++ docroot/themes/contrib/civic/civic-library/components/02-molecules/dropdown-filter/dropdown-filter.js
@@ -5,11 +5,15 @@ function CivicDropdownFilter(el) {
 
   this.el = el;
   this.el.setAttribute('data-dropdown-filter-searchable', '');
+
+  // Options.
   this.dropdownFilterItemSelector = this.el.getAttribute('data-dropdown-filter-item-selector') ?? '.civic-form-element--checkbox,.civic-form-element--radio';
-  this.dropdownFilterItems = this.el.querySelectorAll(this.dropdownFilterItemSelector);
   this.placeholderText = this.el.getAttribute('data-dropdown-filter-placeholder-text') ?? 'Filter by keyword';
+
+  this.dropdownFilterItems = this.el.querySelectorAll(this.dropdownFilterItemSelector);
   this.threshold = this.el.getAttribute('data-dropdown-filter-threshold') ?? 4;
 
+  // Add a search box to the dropdown filter if there are more options than the threshold.
   if (this.dropdownFilterItems.length >= this.threshold) {
     this.init();
   }
@@ -17,21 +21,26 @@ function CivicDropdownFilter(el) {
 
 // eslint-disable-next-line func-names
 CivicDropdownFilter.prototype.init = function () {
+
+  // Create the search box container.
   let search = document.createElement('div');
   search.classList.add('civic-dropdown-filter__search', 'civic-input', 'civic-theme-light');
 
+  // Create the search box element and add it to the container.
   let searchInput = document.createElement('input');
   searchInput.classList.add('civic-dropdown-filter__search__input', 'civic-input__element', 'civic-input--default', 'civic-input--text');
   searchInput.setAttribute('placeholder', this.placeholderText);
   searchInput.setAttribute('value', '');
   searchInput.setAttribute('type', 'text');
   search.append(searchInput);
-
   this.searchInput = searchInput;
+
+  // Add the search box container to the dropdown filter.
   this.el.prepend(search);
+
+  // Add the search box key listener.
   this.keyupListener = this.keyupEvent.bind(this);
   this.searchInput.addEventListener('keyup', this.keyupListener, false);
-
   this.el.setAttribute('data-dropdown-filter-is-initialized', '');
 };
 
