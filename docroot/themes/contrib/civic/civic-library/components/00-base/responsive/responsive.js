5d83916bd83880b988a1d9c06b043a1b2ebef025
Alan <alan@salsadigital.com.au>
2022-03-09 21:40:08 +1100

    [CIVIC-97] Added support for mobile Filters. (#364)
    * [CIVIC-97] Mobile Filters
    
    * [CIVIC-97] Mobile filters - Improve Drupal integration.
    
    * [CIVIC-97] Mobile filters - implement flyout.
    
    * [CIVIC-97] Mobile filters - Share close button.
    
    * [CIVIC-97] Mobile filters collapsible fix.
    * Prevent a "collapse all" event from a different collapsible element from triggering the collapse of a "groups disabled" collapsible element.
    * Mobile nav menu to use default close button.
    * Apply button to allow events to bubble so filters get applied.
    
    * [CIVIC-97] Mobile filters lint fixes.
    
    * [CIVIC-97] Mobile filters fixes.
    * Add JS headers
    * Support plural filter labels.
    * Add Storybook fields for large filter labels.
    
    * [CIVIC-97] Mobile filters fixes.
    * Move colors into variables.components.
    * Support dark theme.
    
    * [CIVIC-97] Mobile filters lint fixes.
    
    * [CIVIC-97] Mobile filters color fixes.
    
    * [CIVIC-97] Mobile filters - Reduce responsive customization.
    
    * [CIVIC-97] Mobile Filters - Breakpoint improvements.
    * Add display modes to dropdown filter.
    * Add better breakpoint support to large filters.
    
    * [CIVIC-97] Mobile Filters - "Auto" display mode for dropdown filters.
    
    * [CIVIC-97] Mobile Filters - Remediations.
    * Collapsible fix to use timeout to avoid DOMContentLoad and window event load.
    * Dropdown filter to use "inline" naming.
    
    * [CIVIC-97] Mobile Filters - Flyout remediation.
    * Allow multiple close / close all selectors in a flyout.
    * Remove custom event handler for apply.
    * Create instance of close button in large-filter.
    
    * [CIVIC-97] Mobile Filters - linting fix.
    
    * [CIVIC-97] Mobile Filters - Integration fixes.
    * Add new data attributes to Drupal twig files.
    * Fix large filter desktop - ensure updateTagContainerPosition runs on first load.
    - Responsive - move mediaQueryChange handler so that it runs any time CivicResponsive runs.
    
    * [CIVIC-97] Mobile Filters - Minor fixes.
    
    * Fix JS.
    
    * Fixed test because of duplicated mobile and desktop buttons.
    
    Co-authored-by: Richard Gaunt <richard.gaunt@protonmail.com>
    Co-authored-by: Alex Skrypnyk <alex@integratedexperts.com>

--- docroot/themes/contrib/civic/civic-library/components/00-base/responsive/responsive.js
+++ docroot/themes/contrib/civic/civic-library/components/00-base/responsive/responsive.js
@@ -20,9 +20,9 @@ function CivicResponsive() {
     if (!(query in window.civicResponsive)) {
       window.civicResponsive[query] = window.matchMedia(query);
       window.civicResponsive[query].addEventListener('change', this.mediaQueryChange.bind(this, breakpoint));
-      // Call event handler on init.
-      this.mediaQueryChange(breakpoint, { matches: window.civicResponsive[query].matches });
     }
+    // Call event handler on init.
+    this.mediaQueryChange(breakpoint, { matches: window.civicResponsive[query].matches });
   }
 }
 
@@ -160,7 +160,15 @@ CivicResponsive.prototype.matchExpr = function (breakpointExpr, breakpoint) {
 };
 
 if (document.querySelectorAll('[data-responsive]').length) {
-  // Init if there is at least a single component with data-responsive
-  // attribute on the page.
-  new CivicResponsive();
+  // CivicResponsive needs to run after all civic-responisve
+  // event listeners have been added.
+  // Delay the execution until after other components have been initialized.
+  // Using setTimeout as an interim solution because:
+  // - DOMContentLoad won't work on prod-site due to being double wrapped in a DOMLoad event.
+  // - window 'load' event won't work on storybook as it's not called per component page change.
+  setTimeout(() => {
+    // Init if there is at least a single component with data-responsive
+    // attribute on the page.
+    new CivicResponsive();
+  }, 10);
 }
