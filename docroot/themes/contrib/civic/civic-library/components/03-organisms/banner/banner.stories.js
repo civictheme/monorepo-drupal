fbf90dbb0a01f647bb0699902e7c06ebbec63783
Alex Skrypnyk <alex@integratedexperts.com>
2022-03-12 16:52:54 +1100

    Updated 'decorative' to 'is_decorative' in Banner.

--- docroot/themes/contrib/civic/civic-library/components/03-organisms/banner/banner.stories.js
+++ docroot/themes/contrib/civic/civic-library/components/03-organisms/banner/banner.stories.js
@@ -32,7 +32,7 @@ export const Banner = (knobTab) => {
       src: demoImage(),
       alt: 'Featured image alt',
     } : null,
-    decorative: boolean('Decorative', true, generalKnobTab),
+    is_decorative: boolean('Decorative', true, generalKnobTab),
     show_breadcrumb: boolean('Show breadcrumb', true, generalKnobTab),
     show_content_text: boolean('Show content text', true, generalKnobTab),
     show_content_search: boolean('Show content search', false, generalKnobTab),
