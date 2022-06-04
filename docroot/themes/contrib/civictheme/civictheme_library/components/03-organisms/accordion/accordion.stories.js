f403c9ed215d5fcc3c3e98fb24af67b7351c785e
Joshua Fernandes <83997348+joshua-salsadigital@users.noreply.github.com>
2022-06-04 10:40:57 +0530

    [CIVIC-651] Updated Accordion to use Collapsible to control state. (#448)

--- docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/accordion/accordion.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/accordion/accordion.stories.js
@@ -2,7 +2,8 @@ import {
   boolean, number, radios, text,
 } from '@storybook/addon-knobs';
 import CivicThemeAccordion from './accordion.twig';
-import './accordion';
+
+import '../../00-base/collapsible/collapsible';
 
 export default {
   title: 'Organisms/Accordion',
