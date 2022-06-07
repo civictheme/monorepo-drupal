5b6ee856808aca7ace822de2a6318ede9fb26de3
Alex Skrypnyk <alex@integratedexperts.com>
2022-06-07 23:30:41 +1000

    Added redirects for Storybook.

--- docroot/modules/custom/cs_core/cs_core.post_update.php
+++ docroot/modules/custom/cs_core/cs_core.post_update.php
@@ -5,6 +5,7 @@
  * Post update hooks for core.
  */
 
+use Drupal\redirect\Entity\Redirect;
 use Drupal\user\Entity\User;
 
 /**
@@ -32,5 +33,32 @@ function cs_core_post_update_provision_users() {
     $user->activate();
     $user->enforceIsNew();
     $user->save();
+  }
+}
+
+/**
+ * Creates storybook redirects.
+ */
+function cs_core_post_update_provision_storybook_redirects() {
+  $map = [
+    [
+      'src' => '/storybook',
+      'dst' => '/themes/contrib/civictheme/civictheme_library/storybook-static/index.html',
+    ],
+    [
+      'src' => '/storybook-drupal',
+      'dst' => '/themes/contrib/civictheme/storybook-static/index.html',
+    ], [
+      'src' => '/storybook-drupal-demo',
+      'dst' => '/themes/custom/civictheme_demo/storybook-static/index.html',
+    ],
+  ];
+
+  foreach ($map as $item) {
+    $redirect = Redirect::create();
+    $redirect->setSource($item['src']);
+    $redirect->setRedirect($item['dst']);
+    $redirect->setStatusCode(301);
+    $redirect->save();
   }
 }
