015a88dfb8da0708e1903930e77c9c66b9e93e86
Alex Skrypnyk <alex@integratedexperts.com>
2022-06-10 16:36:46 +1000

    Added variations for listing and component pages.

--- docroot/modules/custom/cs_generated_content/src/CsGeneratedContentHelper.php
+++ docroot/modules/custom/cs_generated_content/src/CsGeneratedContentHelper.php
@@ -22,4 +22,44 @@ class CsGeneratedContentHelper extends GeneratedContentHelper {
     return self::$assetGenerator->createFromDummyFile($options);
   }
 
+  /**
+   * Get static demo media of the specified bundle.
+   *
+   * @param string $bundle
+   *   Bundle machine name.
+   * @param int $count
+   *   Optional media count to return.
+   * @param int $offset
+   *   Optional offset of the number of media from the beginning.
+   *
+   * @return \Drupal\taxonomy\Entity\Term[]
+   *   Array of media.
+   */
+  public static function staticMedia($bundle, $count = NULL, $offset = 0) {
+    $items = self::$repository->getEntities('media', $bundle);
+    $offset = min(count($items), $offset);
+
+    return !is_null($count) ? array_slice($items, $offset, $count) : $items;
+  }
+
+  /**
+   * Get static demo terms from the specified vocabulary.
+   *
+   * @param string $vid
+   *   Vocabulary machine name.
+   * @param int $count
+   *   Optional term count to return.
+   * @param int $offset
+   *   Optional offset of the number of terms from the beginning.
+   *
+   * @return \Drupal\taxonomy\Entity\Term[]
+   *   Array of terms.
+   */
+  public static function staticTerms($vid, $count = NULL, $offset = 0) {
+    $terms = self::$repository->getEntities('taxonomy_term', $vid);
+    $offset = min(count($terms), $offset);
+
+    return !is_null($count) ? array_slice($terms, $offset, $count) : $terms;
+  }
+
 }
