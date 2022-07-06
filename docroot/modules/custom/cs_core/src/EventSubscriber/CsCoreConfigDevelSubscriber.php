1b976bb046975188f55cab65bdeac21f9c9a1521
Alex Skrypnyk <alex@integratedexperts.com>
2022-07-06 21:04:57 +1000

    Added config filter for permissions.

---
+++ docroot/modules/custom/cs_core/src/EventSubscriber/CsCoreConfigDevelSubscriber.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\cs_core\EventSubscriber;
+
+use Drupal\config_devel\Event\ConfigDevelEvents;
+use Drupal\config_devel\Event\ConfigDevelSaveEvent;
+use Drupal\config_devel\EventSubscriber\ConfigDevelSubscriberBase;
+use Drupal\config_filter\Plugin\ConfigFilterPluginManager;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ConfigManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * CsCoreConfigDevelSubscriber subscriber for ConfigDevel events.
+ *
+ * Integrates custom config_ignore filter into config_devel so that exporting
+ * with config_devel would respect config_ignore filtering.
+ *
+ * Currently limited to a single custom config_ignore filter defined in this
+ * module.
+ */
+class CsCoreConfigDevelSubscriber extends ConfigDevelSubscriberBase implements EventSubscriberInterface {
+
+  /**
+   * The config ignore plugin manager.
+   *
+   * @var \Drupal\config_filter\Plugin\ConfigFilterPluginManager
+   */
+  protected $configFilterPluginManager;
+
+  /**
+   * Constructs the ConfigDevelAutoExportSubscriber object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The configuration factory.
+   * @param \Drupal\Core\Config\ConfigManagerInterface $config_manager
+   *   The configuration manager.
+   * @param \Drupal\config_filter\Plugin\ConfigFilterPluginManager $config_filter_plugin_manager
+   *   The config ignore plugin manager.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ConfigManagerInterface $config_manager, ConfigFilterPluginManager $config_filter_plugin_manager) {
+    parent::__construct($config_factory, $config_manager);
+    $this->configFactory = $config_factory;
+    $this->configManager = $config_manager;
+    $this->configFilterPluginManager = $config_filter_plugin_manager;
+  }
+
+  /**
+   * React to configuration ConfigEvent::SAVE events.
+   *
+   * @param \Drupal\config_devel\Event\ConfigDevelSaveEvent $event
+   *   The event to process.
+   */
+  public function onConfigDevelSave(ConfigDevelSaveEvent $event) {
+    $filenames = $event->getFileNames();
+
+    if (empty($filenames)) {
+      return;
+    }
+
+    $filename = reset($filenames);
+    $name = basename($filename, '.yml');
+    $data = $event->getData();
+
+    /** @var \Drupal\cs_core\Plugin\ConfigFilter\CsCoreIgnoreFilter $plugin */
+    $plugin = $this->configFilterPluginManager->createInstance('cs_core_config_ignore');
+
+    $data = $plugin->filterWrite($name, $data);
+
+    $event->setData($data);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[ConfigDevelEvents::SAVE][] = ['onConfigDevelSave', 10];
+
+    return $events;
+  }
+
+}
