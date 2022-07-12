686603ce7c099d022780e986e9667004e587ffe9
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-07-12 16:42:17 +0530

    [CIVIC-715] Added Webform component to Storybook and add theme/spacing/background.

---
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/webform/webform.stories.js
@@ -0,0 +1,46 @@
+import {
+  boolean, radios, text,
+} from '@storybook/addon-knobs';
+import CivicThemeWebform from './webform.twig';
+import FormElement from '../form-element/form-element.twig';
+
+export default {
+  title: 'Organisms/Webform',
+};
+
+export const Webform = (knobTab) => {
+  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
+  const inputKnobTab = 'Input';
+
+  const theme = radios(
+    'Theme',
+    {
+      Light: 'light',
+      Dark: 'dark',
+    },
+    'light',
+    generalKnobTab,
+  );
+
+  const generalKnobs = {
+    theme,
+    reference_webform: text('Title', 'Webform title', generalKnobTab),
+    with_background: boolean('With background', false, generalKnobTab),
+    vertical_space: radios(
+      'Vertical space',
+      {
+        None: 'none',
+        Top: 'top',
+        Bottom: 'bottom',
+        Both: 'both',
+      },
+      'none',
+      generalKnobTab,
+    ),
+    modifier_class: text('Additional class', '', generalKnobTab),
+  };
+
+  return CivicThemeWebform({
+    ...generalKnobs,
+  });
+};
