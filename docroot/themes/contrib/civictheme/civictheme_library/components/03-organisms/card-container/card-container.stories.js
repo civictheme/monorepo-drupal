33eaa542c777871d0334f33e7ca41eb1b6d48823
Govind Kumar Malu <govind@salsadigital.com.au>
2022-08-01 21:38:44 +0530

    [CIVIC-956] Card container component storybook update.

--- docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/card-container/card-container.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/card-container/card-container.stories.js
@@ -1,13 +1,13 @@
 import {
-  boolean, date, number, radios, text,
+  boolean, number, radios, text,
 } from '@storybook/addon-knobs';
 import CivicThemeCardContainer from './card-container.twig';
-import PromoCard from '../../02-molecules/promo-card/promo-card.twig';
-import NavigationCard from '../../02-molecules/navigation-card/navigation-card.twig';
-import PublicationCard from '../../02-molecules/publication-card/publication-card.twig';
-import ServiceCard from '../../02-molecules/service-card/service-card.twig';
-import SubjectCard from '../../02-molecules/subject-card/subject-card.twig';
-import { demoImage, getSlots, randomTags, randomLinks } from '../../00-base/base.stories';
+import { PromoCard } from '../../02-molecules/promo-card/promo-card.stories';
+import { NavigationCard } from '../../02-molecules/navigation-card/navigation-card.stories';
+import { PublicationCard } from '../../02-molecules/publication-card/publication-card.stories';
+import { ServiceCard } from '../../02-molecules/service-card/service-card.stories';
+import { SubjectCard } from '../../02-molecules/subject-card/subject-card.stories';
+import { getSlots } from '../../00-base/base.stories';
 
 export default {
   title: 'Organisms/Card Container',
@@ -61,6 +61,8 @@ export const CardContainer = (knobTab) => {
     modifier_class: text('Additional class', '', generalKnobTab),
   };
 
+  const cardsKnobTab = 'Cards';
+
   const cardType = radios(
     'Card type',
     {
@@ -69,13 +71,11 @@ export const CardContainer = (knobTab) => {
       'Publication card': 'publication-card',
       'Service card': 'service-card',
       'Subject card': 'subject-card',
-      'Random': 'random',
     },
     'promo-card',
-    generalKnobTab,
+    cardsKnobTab,
   );
 
-  const cardsKnobTab = 'Cards';
   const cardsCount = number(
     'Number of cards',
     4,
@@ -88,100 +88,25 @@ export const CardContainer = (knobTab) => {
     cardsKnobTab,
   );
 
-  const cardsProps = {
-    theme: radios(
-      'Theme',
-      {
-        Light: 'light',
-        Dark: 'dark',
-      },
-      'light',
-      cardsKnobTab,
-    ),
-    date: date('Date', new Date(), cardsKnobTab),
-    title: text('Title', 'Event name which runs across two or three lines', cardsKnobTab),
-    summary: text('Summary', 'Card summary using body copy which can run across multiple lines. Recommend limiting this summary to three or four lines..', cardsKnobTab),
-    url: text('Link URL', 'http://example.com', cardsKnobTab),
-    image: boolean('With image', true, cardsKnobTab) ? {
-      src: demoImage(),
-      alt: 'Image alt text',
-    } : false,
-    modifier_class: text('Additional class', '', cardsKnobTab),
-  };
-
-  if (cardType == 'navigation-card' || cardType == 'promo-card' || cardType == 'random') {
-    cardsProps.tags = randomTags(number(
-      'Number of tags',
-      2,
-      {
-        range: true,
-        min: 0,
-        max: 10,
-        step: 1,
-      },
-      cardsKnobTab,
-    ), true);
-  }
-
-  if (cardType == 'navigation-card' || cardType == 'random') {
-    cardsProps.is_external = boolean('Is external', false, cardsKnobTab);
-  }
-
-  if (cardType == 'service-card' || cardType == 'random') {
-    cardsProps.links = randomLinks(number(
-      'Number of links',
-      5,
-      {
-        range: true,
-        min: 0,
-        max: 10,
-        step: 1,
-      },
-      cardsKnobTab,
-    ), 10);
-  }
-
-  if (cardType == 'publication-card' || cardType == 'random') {
-    cardsProps.link = boolean('With file', true, cardsKnobTab) ? {
-      url: 'https://file-examples-com.github.io/uploads/2017/02/file-sample_100kB.doc',
-      text: 'Filename.pdf (175.96KB)',
-    } : null;
-  }
-
-  cardsProps.date = new Date(cardsProps.date).toLocaleDateString('en-uk', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
-
-  const cardTypes = [
-    NavigationCard(cardsProps),
-    PromoCard(cardsProps),
-    PublicationCard(cardsProps),
-    ServiceCard(cardsProps),
-    SubjectCard(cardsProps),
-  ]
   const cards = [];
   for (let itr = 0; itr < cardsCount; itr += 1) {
+    const cardNumber = `Card ${itr + 1}`;
     switch (cardType) {
-      case "navigation-card":
-        cards.push(cardTypes[0]);
+      case 'navigation-card':
+        cards.push(NavigationCard(cardNumber));
         break;
-      case "promo-card":
-        cards.push(cardTypes[1]);
+      case 'publication-card':
+        cards.push(PublicationCard(cardNumber));
         break;
-      case "publication-card":
-        cards.push(cardTypes[2]);
+      case 'service-card':
+        cards.push(ServiceCard(cardNumber));
         break;
-      case "service-card":
-        cards.push(cardTypes[3]);
+      case 'subject-card':
+        cards.push(SubjectCard(cardNumber));
         break;
-      case "subject-card":
-        cards.push(cardTypes[4]);
-        break;
-      case "random":
+      case 'promo-card':
       default:
-        cards.push(cardTypes[Math.floor(Math.random() * 5)]);
+        cards.push(PromoCard(cardNumber));
     }
   }
 
