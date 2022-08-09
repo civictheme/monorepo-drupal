21b6698b4915fa54c7dc87be12ffb74832d4fc93
Govind Kumar Malu <govind@salsadigital.com.au>
2022-08-09 09:39:44 +0530

    [CIVIC-719] Added listing type field to listing component.

--- docroot/themes/contrib/civictheme/civictheme.api.php
+++ docroot/themes/contrib/civictheme/civictheme.api.php
@@ -11,15 +11,23 @@
  */
 
 use Drupal\views\ViewExecutable;
+use Drupal\paragraphs\Entity\Paragraph;
 
 /**
  * Alter the name of view in CivicTheme listing component.
  *
  * @param string $view
  *   The name of the view used in listing component.
+ * @param \Drupal\paragraphs\Entity\Paragraph $paragraph
+ *   The paragraph containing view conditions.
  */
-function hook_civictheme_listing_view_name_alter(string &$view) {
+function hook_civictheme_listing_view_name_alter(string &$view, string &$display_id, Paragraph $paragraph) {
   $view = 'civictheme_listing';
+  $display_id = 'block1';
+
+  if ($paragraph->hasField('field_c_p_listing_type') && !$paragraph->get('field_c_p_listing_type')->isEmpty()) {
+    list($view, $display_id) = $paragraph->get('field_c_p_listing_type')->getString();
+  }
 }
 
 /**
