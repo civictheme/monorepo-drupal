78018e8ce543e178ef046015d3e78dcf4d9daed9
Alex Skrypnyk <alex@integratedexperts.com>
2022-08-11 18:15:01 +1000

    Added files provisioning and refactored media items to use provisioned files.

--- docroot/modules/custom/cs_generated_content/src/CsGeneratedContentHelper.php
+++ docroot/modules/custom/cs_generated_content/src/CsGeneratedContentHelper.php
@@ -3,7 +3,6 @@
 namespace Drupal\cs_generated_content;
 
 use Drupal\generated_content\Helpers\GeneratedContentHelper;
-use Drupal\file\FileInterface;
 
 /**
  * Class CsGeneratedContentHelper.
@@ -26,41 +25,6 @@ class CsGeneratedContentHelper extends GeneratedContentHelper {
     $entities = static::randomRealEntities('webform', NULL, 1);
 
     return count($entities) > 0 ? reset($entities) : NULL;
-  }
-
-  /**
-   * Get image variation.
-   *
-   * @param \Drupal\file\FileInterface $file
-   *   File entity.
-   * @param int $scale
-   *   Scale constant.
-   *
-   * @return \Drupal\file\FileInterface
-   *   Created managed file.
-   */
-  public static function resizeImagePng(FileInterface $file, $scale) {
-    $file_system = \Drupal::service('file_system');
-    $path = $file_system->realpath($file->getFileUri());
-    $imageinfo = getimagesize($path);
-
-    if (isset($imageinfo[2]) && $imageinfo[2] == IMAGETYPE_PNG) {
-      $image = imagecreatefrompng($path);
-
-      $width = (imagesx($image) > 2000 ? imagesx($image) : 2000) * $scale / 10;
-      $height = (imagesy($image) > 1000 ? imagesx($image) : 1000) * $scale / 10;
-
-      $newimage = imagecreatetruecolor($width, $height);
-      imagecopyresampled($newimage, $image, 0, 0, 0, 0, $width, $height, imagesx($image), imagesy($image));
-      $tmp_file = $file_system->tempnam('temporary://', 'generateImage_');
-      $filepath = $file_system->realpath($tmp_file);
-      imagepng($newimage, $filepath);
-
-      $uri = "public://generated_content/" . basename($path, '.png') . '-' . $scale . '.png';
-      return \Drupal::service('file.repository')->writeData(file_get_contents($filepath), $uri);
-    }
-
-    return FALSE;
   }
 
 }
