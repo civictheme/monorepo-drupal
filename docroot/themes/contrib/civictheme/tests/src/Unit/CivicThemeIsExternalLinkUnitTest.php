d40bcaebdb00c69446cb2e740c81bf7528e18084
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-08-11 13:16:05 +0530

    [CIVIC-959] Code optimization.

--- docroot/themes/contrib/civictheme/tests/src/Unit/CivicThemeIsExternalLinkUnitTest.php
+++ docroot/themes/contrib/civictheme/tests/src/Unit/CivicThemeIsExternalLinkUnitTest.php
@@ -2,72 +2,22 @@
 
 namespace Drupal\Tests\civictheme\Unit;
 
-use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\Extension\ThemeHandlerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-
 /**
  * Class CivicThemeIsExternalLinkUnitTest.
  *
- * Test cases for _civictheme_is_link_external().
+ * Test cases for _civictheme_link_is_external().
  *
  * @group CivicTheme
  */
 class CivicThemeIsExternalLinkUnitTest extends CivicThemeUnitTestBase {
 
   /**
-   * The Drupal service container.
-   *
-   * @var \Drupal\Core\DependencyInjection\Container
-   */
-  protected $container;
-
-
-  /**
-   * @var \Symfony\Component\HttpFoundation\Request|\Prophecy\Prophecy\ProphecyInterface
-   */
-  protected $request;
-
-  /**
-   * @var \Symfony\Component\HttpFoundation\RequestStack|\Prophecy\Prophecy\ProphecyInterface
-   */
-  protected $requestStack;
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp() {
-    parent::setUp();
-
-    $container = new ContainerBuilder();
-
-    // Mock a request.
-    $this->request = $this->prophesize(Request::class);
-
-    // Mock the request_stack service, make it return our mocked request,
-    // and register it in the container.
-    $this->requestStack = $this->prophesize(RequestStack::class);
-    $this->requestStack->getCurrentRequest()->willReturn($this->request->reveal());
-    $container->set('request_stack', $this->requestStack->reveal());
-
-    $config_factory = $this->getConfigFactoryStub([
-      'system.theme' => ['default' => 'civictheme'],
-      'civictheme.settings' => ['civictheme_override_domains' => 'http://overridden.com'],
-    ]);
-    $container->set('config.factory', $config_factory);
-
-    \Drupal::setContainer($container);
-    $this->container = $container;
-  }
-
-  /**
-   * Test for civictheme_is_link_external().
+   * Test for civictheme_link_is_external().
    *
    * @dataProvider dataProviderIsExternalLink
    */
-  public function testParse($url, $expected) {
-    $actual = civictheme_is_link_external($url);
+  public function testParse($url, $host, $overridden_domains, $expected) {
+    $actual = civictheme_link_is_external($url, $host, $overridden_domains);
 
     $this->assertEquals($expected, $actual);
   }
@@ -76,13 +26,24 @@ class CivicThemeIsExternalLinkUnitTest extends CivicThemeUnitTestBase {
    * Data provider for testParse().
    */
   public function dataProviderIsExternalLink() {
+    $host = 'http://test.com';
+    $overridden_domains = [
+      'http://overridden.com',
+      'http://overriddendomain.com',
+    ];
     return [
       // Link without domain.
-      ['/about-us', FALSE],
+      ['/about-us', $host, $overridden_domains, FALSE],
       // Link with an overridden domain.
-      ['http://overridden.com', FALSE],
+      ['http://overridden.com', $host, $overridden_domains, FALSE],
+      // Link with an wildcard overridden domain.
+      [
+        'http://overriddendomain.com/about-us',
+        $host, $overridden_domains,
+        FALSE,
+      ],
       // Link with an external domain.
-      ['http://example.com', TRUE],
+      ['http://example.com', $host, $overridden_domains, TRUE],
     ];
   }
 
