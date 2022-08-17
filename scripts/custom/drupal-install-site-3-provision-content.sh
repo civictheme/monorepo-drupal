ff69e7d6cca255bb26f6fe80553dcb346ac9c5a1
Alex Skrypnyk <alex@integratedexperts.com>
2022-08-18 06:46:15 +1000

    Removed drush alias support from scripts.

--- scripts/custom/drupal-install-site-3-provision-content.sh
+++ scripts/custom/drupal-install-site-3-provision-content.sh
@@ -10,9 +10,6 @@ set -e
 # Path to the application.
 APP="${APP:-/app}"
 
-# Drush alias.
-DRUSH_ALIAS="${DRUSH_ALIAS:-}"
-
 # ------------------------------------------------------------------------------
 
 # Use local or global Drush, giving priority to a local drush.
@@ -20,26 +17,26 @@ drush="$(if [ -f "${APP}/vendor/bin/drush" ]; then echo "${APP}/vendor/bin/drush
 
 if [ "${DREVOPS_DRUPAL_PROFILE}" = "govcms" ]; then
   echo "  > Uninstall obsolete themes."
-  $drush ${DRUSH_ALIAS} -y thun claro || true
-  $drush ${DRUSH_ALIAS} -y thun govcms_bartik || true
-  $drush ${DRUSH_ALIAS} -y thun bartik || true
+  $drush -y thun claro || true
+  $drush -y thun govcms_bartik || true
+  $drush -y thun bartik || true
 
   echo "  > Remove GovCMS configs."
-  $drush ${DRUSH_ALIAS} -y pm-enable civictheme_govcms
-  $drush ${DRUSH_ALIAS} civictheme_govcms:remove-config
+  $drush -y pm-enable civictheme_govcms
+  $drush civictheme_govcms:remove-config
 fi
 
 echo "  > Provision content."
-$drush ${DRUSH_ALIAS} ev -v "require_once '/app/docroot/themes/contrib/civictheme/civictheme.provision.inc'; civictheme_provision_cli();"
+$drush ev -v "require_once '/app/docroot/themes/contrib/civictheme/civictheme.provision.inc'; civictheme_provision_cli();"
 
 echo "  > Provision default content."
-$drush ${DRUSH_ALIAS} -y pm-enable civictheme_content
+$drush -y pm-enable civictheme_content
 
 echo "  > Enable helper module."
-$drush ${DRUSH_ALIAS} -y pm-enable cs_core
+$drush -y pm-enable cs_core
 
 echo "  > Generate test content."
-GENERATED_CONTENT_CREATE=1 $drush ${DRUSH_ALIAS} -y pm-enable cs_generated_content
+GENERATED_CONTENT_CREATE=1 $drush -y pm-enable cs_generated_content
 
 echo "  > Generate sitemap."
-$drush ${DRUSH_ALIAS} simple-sitemap:generate
+$drush simple-sitemap:generate
