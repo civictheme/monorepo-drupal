caa420a90630cf228061c67ea20c08e1f5ee215e
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-08-25 12:34:00 +0530

    [CIVIC-972] drupal_flush_all_caches()

--- scripts/drevops/drupal-install-site.sh
+++ scripts/drevops/drupal-install-site.sh
@@ -181,6 +181,7 @@ else
       echo "  > Existing site content will be removed and new content will be created from the profile."
       $drush cc drush || true
       $drush cr || true
+      $drush eval "drupal_flush_all_caches();" || true
       install_profile
     else
       echo "  > Site content will be preserved."
