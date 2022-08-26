2f86523c6fc16a8c8d9e452a54e2344cc382c1d5
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-08-26 08:55:11 +0530

    [CIVIC-976] Fixed missing menu block values added on build of a child theme.

--- docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/mobile-navigation/mobile-navigation.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/mobile-navigation/mobile-navigation.stories.js
@@ -13,8 +13,8 @@ export default {
 
 export const MobileNavigation = () => {
   const generalKnobTab = 'Menu';
-  const topMenuKnobTab = 'Top menu';
-  const bottomMenuKnobTab = 'Bottom menu';
+  const topMenuKnobTab = 'Top block';
+  const bottomMenuKnobTab = 'Bottom block';
 
   const { icons } = ICONS;
   const defaultIcon = icons.indexOf('content_justifyalignment');
