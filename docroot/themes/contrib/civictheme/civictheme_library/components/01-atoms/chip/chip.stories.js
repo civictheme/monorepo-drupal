ea048278cdb58fb693acd297a0cc29719d433474
Joshua Fernandes <83997348+joshua-salsadigital@users.noreply.github.com>
2022-08-28 15:12:28 +0530

    [CIVIC-980] Extracted Chip component from Button component (#584)

--- docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/chip/chip.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/chip/chip.stories.js
@@ -3,17 +3,17 @@ import {
   boolean, radios, select, text,
 } from '@storybook/addon-knobs';
 
-import CivicThemeButton from './button.twig';
-import './button';
+import CivicThemeChip from './chip.twig';
+import './chip';
 
 export default {
-  title: 'Atoms/Button',
+  title: 'Atoms/Chip',
   parameters: {
     layout: 'centered',
   },
 };
 
-export const Button = (knobTab) => {
+export const Chip = (knobTab) => {
   const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
   const generalKnobs = {
     theme: radios(
@@ -22,16 +22,6 @@ export const Button = (knobTab) => {
         Dark: 'dark',
       },
       'light',
-      generalKnobTab,
-    ),
-    type: radios(
-      'Type', {
-        Primary: 'primary',
-        Secondary: 'secondary',
-        Tertiary: 'tertiary',
-        None: '',
-      },
-      'primary',
       generalKnobTab,
     ),
     size: radios(
@@ -52,6 +42,8 @@ export const Button = (knobTab) => {
         Link: 'link',
         Reset: 'reset',
         Submit: 'submit',
+        Checkbox: 'checkbox',
+        Radio: 'radio',
         None: '',
       },
       'button',
@@ -64,10 +56,10 @@ export const Button = (knobTab) => {
     generalKnobs.is_new_window = boolean('Open in a new window', false, generalKnobTab);
   }
 
-  generalKnobs.text = text('Text', 'Button text', generalKnobTab);
-  generalKnobs.title = text('Title', 'Button Title', generalKnobTab);
+  generalKnobs.text = text('Text', 'Chip text', generalKnobTab);
+  generalKnobs.title = text('Title', 'Chip Title', generalKnobTab);
   generalKnobs.id = text('ID attribute', 'id123', generalKnobTab);
-  generalKnobs.name = text('Name attribute', 'mybutton', generalKnobTab);
+  generalKnobs.name = text('Name attribute', 'mychip', generalKnobTab);
   generalKnobs.is_raw_text = boolean('Allow HTML in text', false, generalKnobTab);
   generalKnobs.is_external = boolean('Is external', false, generalKnobTab);
   generalKnobs.is_disabled = boolean('Disabled', false, generalKnobTab);
@@ -102,5 +94,5 @@ export const Button = (knobTab) => {
     ) : null,
   };
 
-  return CivicThemeButton({ ...generalKnobs, ...iconKnobs });
+  return CivicThemeChip({ ...generalKnobs, ...iconKnobs });
 };
