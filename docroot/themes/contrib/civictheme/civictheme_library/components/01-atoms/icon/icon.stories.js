194d03c889719ea988391a15e98da49894fed5a7
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-08-29 20:35:33 +0530

    [CIVIC-777] Updated Default values for icon component.

--- docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/icon/icon.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/icon/icon.stories.js
@@ -21,33 +21,6 @@ export const Icon = (knobTab) => {
 
   return CivicThemeIcon({
     symbol: select('Symbol', icons, icons[0], generalKnobTab),
-    size: radios('Size', sizes, sizes[0], generalKnobTab),
+    size: radios('Size', sizes, sizes[2], generalKnobTab),
   });
-};
-
-export const IconLibrary = (knobTab) => {
-  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
-
-  const { packs } = ICONS;
-  const sizes = SCSS_VARIABLES['civictheme-icon-sizes'];
-
-  const selectedPack = select('Pack', Object.keys(packs), Object.keys(packs).length ? Object.keys(packs)[0] : null, generalKnobTab);
-
-  let html = `<div class="example-container">`;
-
-  if (selectedPack) {
-    html += `<div class="example-container__title">${selectedPack.charAt(0).toUpperCase()}${selectedPack.slice(1)}</div>`;
-    packs[selectedPack].forEach((icon) => {
-      html += CivicThemeIcon({
-        symbol: icon,
-        size: radios('Size', sizes, sizes[0], generalKnobTab),
-      });
-    });
-
-    html = `<div class="story-icon-wrapper story-wrapper-size--medium">${html}</div>`;
-  }
-
-  html += '</div>';
-
-  return html;
 };
