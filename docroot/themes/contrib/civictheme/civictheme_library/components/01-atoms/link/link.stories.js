0e3131b2cf904342f675c966962adfddeabc4498
Joshua Fernandes <joshua.1234511@yahoo.in>
2022-09-02 08:12:47 +0530

    [CIVIC-783] Updated link storybook component.

--- docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/link/link.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/01-atoms/link/link.stories.js
@@ -1,9 +1,8 @@
 // phpcs:ignoreFile
 import {
-  boolean, radios, text,
+  boolean, radios, text, select,
 } from '@storybook/addon-knobs';
 import CivicThemeLink from './link.twig';
-import { randomUrl } from '../../00-base/base.stories';
 
 export default {
   title: 'Atoms/Link',
@@ -26,16 +25,39 @@ export const Link = (knobTab) => {
       generalKnobTab,
     ),
     text: text('Text', 'Link text', generalKnobTab),
-    title: text('Title', 'Link title', generalKnobTab),
-    hidden_text: text('Link hidden text', 'Link hidden text', generalKnobTab),
-    url: text('URL', randomUrl(), generalKnobTab),
+    url: text('URL', 'https://example.com', generalKnobTab),
     is_external: boolean('Is external', false, generalKnobTab),
     is_new_window: boolean('Open in a new window', false, generalKnobTab),
-    modifier_class: text('Additional class', '', generalKnobTab),
+    with_icon: boolean('With icon', false, generalKnobTab),
     attributes: text('Additional attributes', '', generalKnobTab),
+    modifier_class: text('Additional classes', '', generalKnobTab),
+  };
+
+  const iconKnobTab = 'Icon';
+  const iconKnobs = {
+    icon_type: radios(
+      'Icon type',
+      {
+        'HTML element': 'html',
+        'CSS class': 'css',
+      },
+      'html',
+      iconKnobTab,
+    ),
+    icon_position: radios(
+      'Icon position',
+      {
+        Before: 'before',
+        After: 'after',
+      },
+      'before',
+      iconKnobTab,
+    ),
+    icon: select('Icon', Object.values(ICONS), Object.values(ICONS)[0], iconKnobTab),
   };
 
   return CivicThemeLink({
     ...generalKnobs,
+    ...iconKnobs,
   });
 };
