b3712dbeebb5e8d944e97e6248cd1fc955309556
Joshua Fernandes <83997348+joshua-salsadigital@users.noreply.github.com>
2022-09-12 02:30:02 +0530

    [CIVIC-825] Updated the back to top component. (#661)
    * [CIVIC-825] Updated back to top storybook component.
    
    * [CIVIC-825] Updated back to top storybook component.

--- docroot/themes/contrib/civictheme/civictheme_library/components/02-molecules/back-to-top/back-to-top.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/02-molecules/back-to-top/back-to-top.stories.js
@@ -1,5 +1,4 @@
 // phpcs:ignoreFile
-import { radios } from '@storybook/addon-knobs';
 import CivicThemeBackToTop from './back-to-top.twig';
 
 export default {
@@ -9,24 +8,8 @@ export default {
   },
 };
 
-export const BackToTop = (knobTab) => {
-  const generalKnobTab = typeof knobTab === 'string' ? knobTab : 'General';
-
-  const generalKnobs = {
-    theme: radios(
-      'Theme',
-      {
-        Light: 'light',
-        Dark: 'dark',
-      },
-      'light',
-      generalKnobTab,
-    ),
-  };
-
-  const html = CivicThemeBackToTop({
-    ...generalKnobs,
-  });
+export const BackToTop = () => {
+  const html = CivicThemeBackToTop();
 
   return `<a id="top"></a><div class="example-container"><div class="example-container__page-content example-civictheme-back-to-top"></div>${html}</div>`;
 };
