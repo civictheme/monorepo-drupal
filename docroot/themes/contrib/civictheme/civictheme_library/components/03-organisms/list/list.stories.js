6f921a1e55618685a270c161892b6953d53a0e92
Govind Maloo <govind@salsadigital.com.au>
2022-09-18 04:05:20 +0530

    [CIVIC-1041] Renamed Basic filter to Single filter. (#728)

--- docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/list/list.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/list/list.stories.js
@@ -11,7 +11,7 @@ import {
 } from '../../00-base/base.stories';
 
 import CivicThemeLargeFilter from '../large-filter/large-filter.twig';
-import CivicThemeBasicFilter from '../basic-filter/basic-filter.twig';
+import CivicThemeSingleFilter from '../single-filter/single-filter.twig';
 
 import CivicThemeCardContainer from '../card-container/card-container.twig';
 import PromoCard from '../../02-molecules/promo-card/promo-card.twig';
@@ -187,7 +187,7 @@ export const List = (knobTab) => {
         with_background: withBackground,
       });
     } else {
-      generalKnobs.filters = CivicThemeBasicFilter({
+      generalKnobs.filters = CivicThemeSingleFilter({
         theme,
         is_multiple: false,
         items: filters,
