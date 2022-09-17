9f1dfa72a8d883ae2ad43f89fa2c8b7467c2c4a8
Govind Maloo <govind@salsadigital.com.au>
2022-09-17 12:17:47 +0530

    [CIVIC-1041] Updated filters and toc.

--- docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/table-of-contents/table-of-contents.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/03-organisms/table-of-contents/table-of-contents.stories.js
@@ -6,7 +6,7 @@ import {
 import { randomText } from '../../00-base/base.stories';
 
 export default {
-  title: 'Organisms/TableOfContents',
+  title: 'Organisms/Table Of Contents',
 };
 
 export const TableOfContents = (knobTab) => {
