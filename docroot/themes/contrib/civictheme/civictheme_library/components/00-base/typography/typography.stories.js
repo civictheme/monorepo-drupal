d0e218c1a0ce871a15ec628ee59285a00291947f
Alex Skrypnyk <alex@integratedexperts.com>
2022-09-19 15:31:13 +1000

    Fixed typography story.

--- docroot/themes/contrib/civictheme/civictheme_library/components/00-base/typography/typography.stories.js
+++ docroot/themes/contrib/civictheme/civictheme_library/components/00-base/typography/typography.stories.js
@@ -2,27 +2,8 @@
 
 import CivicThemeTypography from './typography.stories.twig';
 
-const merge = require('deepmerge');
-
 export default {
   title: 'Base/Typography',
 };
 
-export const Typography = () => {
-  const defaultValues = SCSS_VARIABLES['ct-typography-default'];
-  const customValues = SCSS_VARIABLES['ct-typography'];
-  const typographyNames = Object.keys(merge(defaultValues, customValues));
-
-  let html = '';
-
-  for (const i in Object.keys(typographyNames)) {
-    html += `<div class="example-container">`;
-    html += `<div class="example-container__title">${typographyNames[i].charAt(0).toUpperCase()}${typographyNames[i].replace('-', ' ').slice(1)} <code>${typographyNames[i]}</code></div>`;
-    html += `<div class="example-container__content ct-${typographyNames[i]}">The quick brown fox jumps over the lazy dog</div>`;
-    html += `</div>`;
-  }
-
-  return CivicThemeTypography({
-    html,
-  });
-};
+export const Typography = () => CivicThemeTypography();
