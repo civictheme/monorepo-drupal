621e65ce77a973156e521d406ec1fb62ef05ee01
Alex Skrypnyk <alex@integratedexperts.com>
2022-10-11 10:30:08 +1100

    Fixed theme dependencies and switched CI build to use minimal profile instead of standard.

--- scripts/custom/drupal-install-site-1-enable-modules.sh
+++ scripts/custom/drupal-install-site-1-enable-modules.sh
@@ -19,13 +19,22 @@ echo "==> Removing all files."
 rm -Rf "${APP}"/docroot/sites/default/files/* >/dev/null || true
 
 echo "  > Enable modules required by CivicTheme."
-$drush ev "require_once dirname(\Drupal::getContainer()->get('theme_handler')->rebuildThemeData()['civictheme']->getPathname()) . '/theme-settings.provision.inc'; civictheme_enable_modules();"
+$drush php:eval "require_once dirname(\Drupal::getContainer()->get('theme_handler')->rebuildThemeData()['civictheme']->getPathname()) . '/theme-settings.provision.inc'; civictheme_enable_modules();"
 
 echo "  > Enable admin theme and set as default."
-$drush -y then adminimal_theme
-$drush -y config-set system.theme admin adminimal_theme
+if [ "${DREVOPS_DRUPAL_PROFILE}" = "govcms" ]; then
+  $drush -y theme-enable adminimal_theme
+  $drush -y config-set system.theme admin adminimal_theme
+  $drush -y theme-uninstall claro || true
+  $drush -y theme-uninstall govcms_bartik || true
+  $drush -y theme-uninstall bartik || true
+elif [ "${DREVOPS_DRUPAL_PROFILE}" = "minimal" ]; then
+  $drush -y theme-enable claro
+  $drush -y config-set system.theme admin claro
+  $drush -y config-set node.settings use_admin_theme 1
+fi
 
 echo "  > Enable CivicTheme theme and set as default."
-$drush -y then civictheme
+$drush -y theme-enable civictheme
 $drush -y config-set system.theme default civictheme
 $drush -y config-set media.settings standalone_url true
