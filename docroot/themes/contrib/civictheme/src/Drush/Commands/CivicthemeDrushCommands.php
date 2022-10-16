dd441d7d05fa7a794e19e0a7a7c361e77998b645
Govind Kumar Malu <govind@salsadigital.com.au>
2022-10-17 10:37:05 +1100

    [CIVIC] Custom Drush command for theme.

---
+++ docroot/themes/contrib/civictheme/src/Drush/Commands/CivicthemeDrushCommands.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CivicTheme\Drush\Commands;
+
+use Drupal\civictheme\CivicthemeColorManager;
+use Drush\Commands\DrushCommands;
+
+class CivicthemeDrushCommands extends DrushCommands {
+
+  /**
+   * Color manager.
+   *
+   * @var \Drupal\civictheme\CivicthemeColorManager
+   */
+  protected $colorManager;
+
+  /**
+   * Prints the current alias name and info.
+   *
+   * @command civictheme:sbc
+   */
+  public function setBrandColors($light_brand1, $light_brand2, $light_brand3, $dark_brand1, $dark_brand2, $dark_brand3) {
+    $this->colorManager = \Drupal::classResolver(CivicthemeColorManager::class);
+
+    $this->colorManager->updateColors($light_brand1, $light_brand2, $light_brand3, $dark_brand1, $dark_brand2, $dark_brand3);
+  }
+
+}
