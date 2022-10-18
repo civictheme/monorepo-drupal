7cd4881d8f5a713ea36e83be133a22acd2bf72f6
Joshua Fernandes <joshua.fernandes@salsadigital.com.au>
2022-10-18 14:47:43 +0530

    [CIVIC-1119] Added unit test for Color utility.

---
+++ docroot/themes/contrib/civictheme/tests/src/Unit/CivicthemeColorUtilityUnitTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Drupal\Tests\civictheme\Unit;
+
+use Drupal\civictheme\Color\CivicthemeColorUtility;
+
+/**
+ * Class CivicthemeColorUtilityUnitTest.
+ *
+ * Test cases for color utility functions.
+ *
+ * @group CivicTheme
+ */
+class CivicthemeColorUtilityUnitTest extends CivicThemeUnitTestBase {
+
+  /**
+   * Test for normalizeHex().
+   *
+   * @dataProvider dataProviderNormalizeHex
+   * @SuppressWarnings(PHPMD.StaticAccess)
+   */
+  public function testNormalizeHex($value, $preserve_hash, $expected) {
+    $actual = CivicthemeColorUtility::normalizeHex($value, $preserve_hash);
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for testNormalizeHex().
+   */
+  public function dataProviderNormalizeHex() {
+    return [
+      ['#00698f', FALSE, '00698f'],
+      ['#000', FALSE, '000000'],
+      ['fff', TRUE, 'ffffff'],
+      ['00698f', TRUE, '00698f'],
+      ['61daff', FALSE, '61daff'],
+    ];
+  }
+
+  /**
+   * Test for mix().
+   *
+   * @dataProvider dataProviderMix
+   * @SuppressWarnings(PHPMD.StaticAccess)
+   */
+  public function testMix($color, $mixer, $range, $expected) {
+    $actual = CivicthemeColorUtility::mix($color, $mixer, $range);
+    $this->assertEquals($expected, $actual);
+  }
+
+  /**
+   * Data provider for testMix().
+   */
+  public function dataProviderMix() {
+    return [
+      ['#00698f', '#000', '10', '#005e80'],
+      ['#00698f', '#000', '', '#00698f'],
+      ['#e6e9eb', '#fff', '80', '#fafafb'],
+    ];
+  }
+
+}
