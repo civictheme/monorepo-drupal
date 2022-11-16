077785ebe79c89c4621159b007e88088d4728a2e
Alex Skrypnyk <alex@drevops.com>
2022-11-16 19:12:16 +1100

    Updated config.

--- docroot/modules/custom/civictheme_content/modules/civictheme_content_government/civictheme_content_government.post_update.php
+++ docroot/modules/custom/civictheme_content/modules/civictheme_content_government/civictheme_content_government.post_update.php
@@ -2,13 +2,20 @@
 
 /**
  * @file
- * Post update hooks for CivicTheme Default Government Content.
+ * Post update hooks for CivicTheme Government Content.
  */
 
 use Drupal\block\BlockInterface;
 use Drupal\civictheme_content\Helper;
 use Drupal\Core\Config\Entity\ConfigEntityUpdater;
 use Drupal\Core\Utility\UpdateException;
+
+/**
+ * Common updates.
+ */
+function civictheme_content_government_post_update_common() {
+  civictheme_content_post_update_common();
+}
 
 /**
  * Sets homepage.
@@ -22,34 +29,4 @@ function civictheme_content_government_post_update_set_homepage() {
   catch (\Exception $e) {
     throw new UpdateException($e->getMessage());
   }
-}
-
-/**
- * Provisions links in footer and updates menu block configurations.
- *
- * @SuppressWarnings(PHPMD.StaticAccess)
- */
-function civictheme_content_government_post_update_provision_footer_links() {
-  $theme = \Drupal::theme()->getActiveTheme()->getName();
-
-  $map = [
-    $theme . '_footer_menu_1' => Helper::findMenuItemByTitle('civictheme-footer', 'Department of Citizens'),
-    $theme . '_footer_menu_2' => Helper::findMenuItemByTitle('civictheme-footer', 'Help'),
-    $theme . '_footer_menu_3' => Helper::findMenuItemByTitle('civictheme-footer', 'Resources'),
-    $theme . '_footer_menu_4' => Helper::findMenuItemByTitle('civictheme-footer', 'Other'),
-  ];
-
-  $sandbox = [];
-  \Drupal::classResolver(ConfigEntityUpdater::class)
-    ->update($sandbox, 'block', function (BlockInterface $block) use ($map) {
-      if (strpos($block->getPluginId(), 'menu_block:') === 0) {
-        if (!empty($map[$block->id()])) {
-          $block_settings = $block->get('settings');
-          $block_settings['parent'] = 'civictheme-footer:menu_link_content:' . $map[$block->id()]->uuid();
-          $block->set('settings', $block_settings);
-          return TRUE;
-        }
-      }
-      return FALSE;
-    });
 }
