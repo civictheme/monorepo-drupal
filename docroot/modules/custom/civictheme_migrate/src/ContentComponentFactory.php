c5b35f9291a8e4f4c243318dff47f5856c2fd6bf
Govind Malu <govind@salsadigital.com.au>
2023-03-15 16:49:33 +0530

    [1032] Migration page and related components.

---
+++ docroot/modules/custom/civictheme_migrate/src/ContentComponentFactory.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace Drupal\civictheme_migrate;
+
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\civictheme_migrate\Utils\CivicthemeTrait;
+use Drupal\migrate\Plugin\MigratePluginManager;
+use Drupal\paragraphs\ParagraphInterface;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+
+/**
+ * Content Component Factory.
+ *
+ * @package Drupal\civictheme_migrate
+ */
+class ContentComponentFactory {
+  use CivicthemeTrait;
+
+  /**
+   * The MigratePluginManager instance.
+   *
+   * @var \Drupal\migrate\Plugin\MigratePluginManagerInterface
+   */
+  protected $migratePluginManager;
+
+  /**
+   * Entity Type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * ContentComponentFactory constructor.
+   *
+   * @param \Drupal\migrate\Plugin\MigratePluginManager $migrate_plugin_manager
+   *   Migrate plugin manager.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager.
+   */
+  public function __construct(MigratePluginManager $migrate_plugin_manager, Connection $database, EntityTypeManagerInterface $entity_type_manager) {
+    $this->migratePluginManager = $migrate_plugin_manager;
+    $this->database = $database;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Create components.
+   *
+   * @param mixed $items
+   *   The list of items to create.
+   * @param array $context
+   *   The migration context array with 4 keys:
+   *    - configuration: the configuration of the current field.
+   *    - migration: the current migrate executable.
+   *    - row: the current row.
+   *    - destination_property: destination property currently worked on.
+   *
+   * @return array|null
+   *   The components.
+   */
+  public function createComponents($items, array &$context) {
+    if (!is_array($items)) {
+      return NULL;
+    }
+
+    $components = [];
+    foreach ($items as $item) {
+      foreach ($item as $item_type => $item_data) {
+        $component = $this->createComponent($item_type, $item_data, $context);
+        if ($component instanceof ParagraphInterface) {
+          $components[] = $component;
+        }
+        elseif (is_array($component)) {
+          foreach ($component as $component_item) {
+            if ($component_item instanceof ParagraphInterface) {
+              $components[] = $component_item;
+            }
+          }
+        }
+      }
+    }
+
+    return $components;
+  }
+
+  /**
+   * Create a single component.
+   *
+   * @param string $item_type
+   *   The type of the component to create.
+   * @param mixed $item_data
+   *   The item data to create the component.
+   * @param array $context
+   *   The migration context.
+   *
+   * @return mixed|null
+   *   The component.
+   */
+  public function createComponent(string $item_type, $item_data, array &$context) {
+    $method = $this->getFactoryMethod($item_type);
+    if ($method) {
+      return call_user_func_array([$this, $method], [$item_data, &$context]);
+    }
+    return NULL;
+  }
+
+  /**
+   * Get the factory method for an item type.
+   *
+   * @param string $item_type
+   *   The item type.
+   *
+   * @return string|null
+   *   The method.
+   */
+  protected function getFactoryMethod($item_type) : ?string {
+    $converter = new CamelCaseToSnakeCaseNameConverter();
+    $method = (string) $converter->denormalize('generate_' . $item_type);
+    return is_callable([$this, $method]) ? $method : NULL;
+  }
+
+  /**
+   * Produce Basic Text component.
+   *
+   * @param mixed $item_data
+   *   The item data to create the component.
+   * @param array $context
+   *   The migration context.
+   *
+   * @return \Drupal\paragraphs\ParagraphInterface|null
+   *   The component.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function generateTextContent($item_data, array &$context) : ?ParagraphInterface {
+    if (!empty($item_data['value'])) {
+      $options = [
+        'content' => $item_data['value'],
+      ];
+      return $this->civicthemeComponentCreate('content', $options);
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Produce Manual list components.
+   *
+   * @param mixed $item_data
+   *   The item data to create the component.
+   * @param array $context
+   *   The migration context.
+   *
+   * @return \Drupal\paragraphs\ParagraphInterface|null
+   *   The component.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function generateList($item_data, array &$context): ?ParagraphInterface {
+    if (!empty($item_data['children'])) {
+      $cards = [];
+      if (!empty($item_data['children']['cards']['children'])) {
+        $type = "civictheme_" . $item_data['children']['cards']['type'];
+        foreach ($item_data['children']['cards']['children'] as $children) {
+          $cards[] = [
+            'type' => $type,
+            'title' => $children['item_title'],
+            'summary' => $children['item_content']['value'],
+          ];
+        }
+      }
+
+      $options = [
+        'cards' => $cards,
+      ];
+
+      return $this->civicthemeComponentCreate('manual_list', $options);
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Produce Accordion components.
+   *
+   * @param mixed $item_data
+   *   The item data to create the component.
+   * @param array $context
+   *   The migration context.
+   *
+   * @return \Drupal\paragraphs\ParagraphInterface|null
+   *   The component.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  protected function generateAccordion($item_data, array &$context) : ?ParagraphInterface {
+    if (!empty($item_data['children'])) {
+      $accordion_items = [];
+      if (!empty($item_data['children']['accordion_list']['children'])) {
+        foreach ($item_data['children']['accordion_list']['children'] as $items) {
+          $children = NestedArray::getValue($items,
+            ['accordion_items', 'children']);
+          foreach ($children as $child) {
+            $accordion_items[] = [
+              'title' => $child['item_title'],
+              'content' => $child['item_content']['value'],
+            ];
+          }
+        }
+      }
+
+      $options = [
+        'panels' => $accordion_items,
+      ];
+
+      return $this->civicthemeComponentCreate('accordion', $options);
+    }
+    return NULL;
+  }
+
+}
---
+++ docroot/modules/custom/civictheme_migrate/src/Plugin/migrate/process/ContentComponents.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\civictheme_migrate\Plugin\migrate\process;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\civictheme_migrate\ContentComponentFactory;
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\Plugin\MigratePluginManager;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+use Drupal\paragraphs\ParagraphInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Content Components creator.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "ct_content_component_create",
+ *   handle_multiples = TRUE
+ * )
+ *
+ * @package Drupal\civictheme_migrate\Plugin\migrate\process
+ */
+class ContentComponents extends ProcessPluginBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Content Component Factory.
+   *
+   * @var \Drupal\civictheme_migrate\ContentComponentFactory
+   */
+  protected $factory;
+
+  /**
+   * The MigratePluginManager instance.
+   *
+   * @var \Drupal\migrate\Plugin\MigratePluginManagerInterface
+   */
+  protected $migratePluginManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ContentComponentFactory $factory, MigratePluginManager $migrate_plugin_manager, Connection $database) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->factory = $factory;
+    $this->migratePluginManager = $migrate_plugin_manager;
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('civictheme_migrate.migration.content_component_factory'),
+      $container->get('plugin.manager.migrate.process'),
+      $container->get('database')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    if (!is_array($value)) {
+      return NULL;
+    }
+
+    $items = [];
+    $context = [
+      'configuration' => $this->configuration,
+      'migration' => $migrate_executable,
+      'row' => clone $row,
+      'destination_property' => $destination_property,
+    ];
+
+    $paragraphs = $this->factory->createComponents($value, $context);
+    if ($paragraphs) {
+      foreach ($paragraphs as $paragraph) {
+        if ($paragraph instanceof ParagraphInterface) {
+          $items[] = $paragraph;
+        }
+      }
+    }
+
+    return $items;
+  }
+
+  /**
+   * Transform a destination value.
+   *
+   * @param mixed $value
+   *   The value to transform.
+   * @param \Drupal\migrate\MigrateExecutableInterface $migrate_executable
+   *   The current migration.
+   * @param \Drupal\migrate\Row $row
+   *   The current row.
+   *
+   * @return mixed
+   *   The transformed value.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
+  protected function getTransformedValue($value, MigrateExecutableInterface $migrate_executable, Row $row) {
+    if (is_array($value)) {
+      $plugin = $this->migratePluginManager->createInstance($value['plugin'], $value);
+    }
+    else {
+      $plugin = $this->migratePluginManager->createInstance('get', ['source' => $value]);
+    }
+    return $plugin->transform(NULL, $migrate_executable, $row, $value);
+  }
+
+}
