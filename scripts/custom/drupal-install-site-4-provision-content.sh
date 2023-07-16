4866b15030419d906d23c0c01322bcbeef6da97d
Alex Skrypnyk <alex@drevops.com>
2023-07-16 16:47:00 +1000

    Updated SKIP* variables.
    Signed-off-by: Alex Skrypnyk <alex@drevops.com>

--- scripts/custom/drupal-install-site-4-provision-content.sh
+++ scripts/custom/drupal-install-site-4-provision-content.sh
@@ -15,15 +15,6 @@ APP="${APP:-/app}"
 # Use local or global Drush, giving priority to a local drush.
 drush="$(if [ -f "${APP}/vendor/bin/drush" ]; then echo "${APP}/vendor/bin/drush"; else command -v drush; fi)"
 
-if [ "${DREVOPS_DRUPAL_PROFILE}" = "govcms" ]; then
-  echo "  > Remove GovCMS configs."
-  $drush -y pm-enable civictheme_govcms
-  $drush civictheme_govcms:remove-config
-else
-  echo "  > Enable Admin module."
-  $drush -y pm-enable civictheme_admin
-fi
-
 if [ -n "${CIVICTHEME_CONTENT_PROFILE}" ]; then
   echo "  > Provisioning content from \"${CIVICTHEME_CONTENT_PROFILE}\" content profile."
   $drush -y pm-enable civictheme_content
@@ -32,11 +23,7 @@ else
   $drush php:eval -v "require_once '/app/docroot/themes/contrib/civictheme/theme-settings.provision.inc'; civictheme_provision_cli();"
 fi
 
-echo "  > Enable development module."
-$drush -y pm-enable civictheme_dev
-
-if [ "${CIVICTHEME_SKIP_GENERATED_CONTENT_CREATE}" != "1" ]; then
-
+if [ "${CIVICTHEME_GENERATED_CONTENT_CREATE_SKIP}" != "1" ]; then
   echo "  > Generate test content."
   GENERATED_CONTENT_CREATE=1 $drush -y pm-enable cs_generated_content
 
--- scripts/drevops/lint.sh
+++ scripts/drevops/lint.sh
@@ -63,7 +63,7 @@ fi
 # Lint theme configuration.
 if [ -z "${DREVOPS_LINT_TYPE##*config*}" ]; then
   # Lint theme configuration.
-  ./scripts/lint-theme-config.sh
+  ./scripts/lint-theme-config.sh || [ "${DREVOPS_LINT_CONFIG_ALLOW_FAILURE:-0}" -eq 1 ]
 
   # Lint theme schema.
   ./vendor/bin/drush inspect_config civictheme.settings --detail --only-error
