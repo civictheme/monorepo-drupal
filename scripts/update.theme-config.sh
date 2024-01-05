83b4765b5194fa2eac650f9fa08e0e41aedc6533
Alex Skrypnyk <alex@drevops.com>
2024-01-05 20:57:50 +1100

    Renamed custom scripts.

--- scripts/update.theme-config.sh
+++ scripts/update.theme-config.sh
@@ -3,14 +3,14 @@
 # Update theme config from the current site config.
 #
 # Usage:
-# ./scripts/update-theme-config.sh
+# ./scripts/update.theme-config.sh
 #
 # Caveat: configs in 'optional' section should be added to
 # the $EXCLUDED_CONFIGS_FILE file.
 #
 
-set -e
-[ -n "${DREVOPS_DEBUG}" ] && set -x
+set -eu
+[ -n "${DREVOPS_DEBUG:-}" ] && set -x
 
 # File with exclusion patterns.
 EXCLUDED_CONFIGS_FILE="scripts/theme_excluded_configs.txt"
@@ -41,15 +41,15 @@ echo "==> "
 [ ! -f "${THEME_INFO}" ] && "ERROR: ${THEME_INFO} does not exist"
 
 # Remove temp directories that may exist from the previous run.
-rm -Rf "${TMP_DIR_EXPORTED:?}" > /dev/null || true
-rm -Rf "${TMP_FILE:?}" > /dev/null || true
+rm -Rf "${TMP_DIR_EXPORTED:?}" >/dev/null || true
+rm -Rf "${TMP_FILE:?}" >/dev/null || true
 
 # Create temp dirs.
-mkdir -p "${TMP_DIR_EXPORTED}" > /dev/null
+mkdir -p "${TMP_DIR_EXPORTED}" >/dev/null
 
 echo "  > Loading excluded patterns from ${EXCLUDED_CONFIGS_FILE}"
 excluded_config_patterns=()
-while IFS= read -r; do excluded_config_patterns+=("$REPLY"); done < "${EXCLUDED_CONFIGS_FILE}"
+while IFS= read -r; do excluded_config_patterns+=("$REPLY"); done <"${EXCLUDED_CONFIGS_FILE}"
 
 echo "  > Exporting current config to ${TMP_DIR_EXPORTED}"
 drush -q cex -y --destination="${TMP_DIR_EXPORTED}"
@@ -58,16 +58,16 @@ echo "  > Removing excluded config items"
 for exclude_pattern in "${excluded_config_patterns[@]}"; do
   # shellcheck disable=SC2086
   rm -f "${TMP_DIR_EXPORTED}"/${exclude_pattern}.yml || true
-done;
+done
 
 echo "  > Compiling a list of theme configs"
-echo "  install:" >> "${TMP_FILE}"
+echo "  install:" >>"${TMP_FILE}"
 for file in "${TMP_DIR_EXPORTED}"/*; do
   filename="$(basename "$file")"
   filename=${filename%".yml"}
-  echo "    - $filename" >> "${TMP_FILE}"
+  echo "    - $filename" >>"${TMP_FILE}"
 done
-echo "  optional:" >> "${TMP_FILE}"
+echo "  optional:" >>"${TMP_FILE}"
 
 echo "  > Writing config to theme info ${THEME_INFO}"
 sed -i -e '/install:/,/optional:/!b' -e "/optional:/!d;r ${TMP_FILE}" -e 'd' "${THEME_INFO}"
@@ -77,8 +77,8 @@ theme_dir="$(dirname ${THEME_INFO})"
 theme_config_install_dir="${theme_dir}/config/install"
 theme_config_optional_dir="${theme_dir}/config/optional"
 echo "  > Removing existing theme configs."
-rm "${theme_config_install_dir}"/* > /dev/null
-rm "${theme_config_optional_dir}"/* > /dev/null
+rm "${theme_config_install_dir}"/* >/dev/null
+rm "${theme_config_optional_dir}"/* >/dev/null
 
 echo "  > Exporting theme configs"
 drush -q cde civictheme
