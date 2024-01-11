07c6089010bc63fdb67ff945719595d84b1b2836
Alex Skrypnyk <alex@drevops.com>
2024-01-11 11:03:52 +1100

    Fixed test related to removal of the outdated UIKit version identification.

--- tests/phpunit/CivicthemeCreateSubthemeScriptUnitTest.php
+++ tests/phpunit/CivicthemeCreateSubthemeScriptUnitTest.php
@@ -138,7 +138,6 @@ class CivicthemeCreateSubthemeScriptUnitTest extends ScriptUnitTestBase {
     $this->assertFileExists($expected_new_theme_dir_full . 'screenshot.png');
 
     $this->assertStringContainsString($expected_rel_path, (string) file_get_contents($expected_new_theme_dir_full . 'gulpfile.js'));
-    $this->assertStringContainsString($expected_rel_path, (string) file_get_contents($expected_new_theme_dir_full . 'webpack/webpack.common.js'));
     $this->assertStringContainsString($expected_rel_path, (string) file_get_contents($expected_new_theme_dir_full . 'webpack/theme_js.js'));
 
     $composerjson_actual = json_decode((string) file_get_contents($expected_new_theme_dir_full . 'composer.json'), TRUE);
