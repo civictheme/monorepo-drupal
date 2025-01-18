a0dc51f368ad0b1f0df2c34dc3c80af8e1fdcd4e
Alan Cole <alan.cole@salsa.digital>
2025-01-18 23:04:11 +1100

    [CIVIC-1947] Added updated nav card stories file.

--- web/themes/contrib/civictheme/civictheme_starter_kit/components/02-molecules/navigation-card/navigation-card.stories.js
+++ web/themes/contrib/civictheme/civictheme_starter_kit/components/02-molecules/navigation-card/navigation-card.stories.js
@@ -1,75 +1,67 @@
-import CivicThemeNavigationCard from './navigation-card.twig';
-import { demoImage, knobBoolean, knobRadios, knobSelect, knobNumber, knobText, randomUrl, shouldRender, slotKnobs, randomTags } from '../../00-base/storybook/storybook.utils';
+import Component from './navigation-card.twig';
+import NavigationCardData from './navigation-card.stories.data';
+import Constants from '../../../dist/constants.json'; // eslint-disable-line import/no-unresolved
 
-export default {
+const meta = {
   title: 'Molecules/List/Navigation Card',
-  parameters: {
-    layout: 'centered',
-    storyLayoutSize: 'medium',
-    storyLayoutIsContainer: true,
-    storyLayoutIsResizable: true,
-    docs: 'This component adapts to the width of its container instead of the viewport. Use the toolbar button to enable container resizing.',
-    docsPlacement: 'after',
-    docsSize: 'small',
+  component: Component,
+  argTypes: {
+    theme: {
+      control: { type: 'radio' },
+      options: ['light', 'dark'],
+    },
+    title: {
+      control: { type: 'text' },
+    },
+    summary: {
+      control: { type: 'text' },
+    },
+    link: {
+      control: { type: 'object' },
+    },
+    image: {
+      control: { type: 'object' },
+    },
+    image_as_icon: {
+      control: { type: 'boolean' },
+    },
+    // This is a new property added for this extended component.
+    tags: {
+      control: { type: 'number', min: 0, max: 10, step: 1 },
+    },
+    icon: {
+      control: { type: 'select' },
+      options: Constants.ICONS,
+    },
+    image_over: {
+      control: { type: 'text' },
+    },
+    content_top: {
+      control: { type: 'text' },
+    },
+    content_middle: {
+      control: { type: 'text' },
+    },
+    content_bottom: {
+      control: { type: 'text' },
+    },
+    modifier_class: {
+      control: { type: 'text' },
+    },
+    attributes: {
+      control: { type: 'text' },
+    },
   },
 };
 
-export const NavigationCard = (parentKnobs = {}) => {
-  const knobs = {
-    theme: knobRadios(
-      'Theme',
-      {
-        Light: 'light',
-        Dark: 'dark',
-      },
-      'light',
-      parentKnobs.theme,
-      parentKnobs.knobTab,
-    ),
-    title: knobText('Title', 'Navigation card heading which runs across two or three lines', parentKnobs.title, parentKnobs.knobTab),
-    summary: knobText('Summary', 'Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.', parentKnobs.summary, parentKnobs.knobTab),
-    link: knobBoolean('With link', true, parentKnobs.with_link, parentKnobs.knobTab) ? {
-      url: knobText('Link URL', randomUrl(), parentKnobs.link_url, parentKnobs.knobTab),
-      is_external: knobBoolean('Link is external', false, parentKnobs.link_is_external, parentKnobs.knobTab),
-      is_new_window: knobBoolean('Open in a new window', false, parentKnobs.link_is_new_window, parentKnobs.knobTab),
-    } : null,
-    image: knobBoolean('With image', true, parentKnobs.with_image, parentKnobs.knobTab) ? {
-      url: demoImage(),
-      alt: 'Image alt text',
-    } : null,
-    image_as_icon: knobBoolean('Image as icon', false, parentKnobs.image_as_icon, parentKnobs.knobTab),
-    // This is a new property added for this extended component.
-    tags: randomTags(knobNumber(
-      'Number of tags',
-      2,
-      {
-        range: true,
-        min: 0,
-        max: 10,
-        step: 1,
-      },
-      parentKnobs.tags,
-      parentKnobs.knobTab,
-    ), true),
-    modifier_class: knobText('Additional class', '', parentKnobs.modifier_class, parentKnobs.knobTab),
-    attributes: knobText('Additional attributes', '', parentKnobs.attributes, parentKnobs.knobTab),
-  };
+export default meta;
 
-  const iconKnobTab = 'Icon';
-  const withIcon = knobBoolean('With icon', false, parentKnobs.with_icon, parentKnobs.knobTab);
-  const iconKnobs = {
-    icon: withIcon ? knobSelect('Icon', Object.values(ICONS), Object.values(ICONS)[0], parentKnobs.icon, iconKnobTab) : null,
-  };
-
-  const combinedKnobs = { ...knobs, ...iconKnobs };
-
-  return shouldRender(parentKnobs) ? CivicThemeNavigationCard({
-    ...combinedKnobs,
-    ...slotKnobs([
-      'image_over',
-      'content_top',
-      'content_middle',
-      'content_bottom',
-    ]),
-  }) : combinedKnobs;
+export const NavigationCard = {
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    ...NavigationCardData.args('light'),
+    tags: 2,
+  }
 };
