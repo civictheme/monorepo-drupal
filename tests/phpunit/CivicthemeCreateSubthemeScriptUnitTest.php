f9206083766a8d2bfea36b4f821417f6a5d5caa1
Richard Gaunt <richard.gaunt@protonmail.com>
2025-02-21 15:06:21 +1100

    Try new directory location.

--- tests/phpunit/CivicthemeCreateSubthemeScriptUnitTest.php
+++ tests/phpunit/CivicthemeCreateSubthemeScriptUnitTest.php
@@ -18,14 +18,14 @@ class CivicthemeCreateSubthemeScriptUnitTest extends ScriptUnitTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $script = '/themes/contrib/civictheme/civictheme_create_subtheme.php';
+  protected $script = 'themes/contrib/civictheme/civictheme_create_subtheme.php';
 
   /**
    * CivicTheme dir.
    *
    * @var string
    */
-  protected $civicthemeDir = '/themes/contrib/civictheme';
+  protected $civicthemeDir = 'themes/contrib/civictheme';
 
   /**
    * @dataProvider dataProviderMain
