46eb19867d23da09a1a81120754b2537233cddba
rgaunt <richard.gaunt@protonmail.com>
2025-05-06 10:08:45 +1000

    Fix starter kit for sub-theme, updated asset tests for sub-theme.

--- web/themes/contrib/civictheme/civictheme_starter_kit/vite.config.js
+++ web/themes/contrib/civictheme/civictheme_starter_kit/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import { resolve, parse } from 'path';
+import { resolve } from 'path';
 import twig from 'vite-plugin-twig-drupal';
 import sdcPlugin from './.storybook/sdc-plugin.js';
-
 const componentDir = resolve(import.meta.dirname, './components_combined');
 const coreComponentDir = resolve(import.meta.dirname, './.components-civictheme');
 
@@ -10,18 +9,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     twig({
       namespaces: {
-        'base': resolve(componentDir, './00-base'),
-        'atoms': resolve(componentDir, './01-atoms'),
-        'molecules': resolve(componentDir, './02-molecules'),
-        'organisms': resolve(componentDir, './03-organisms'),
-        'templates': resolve(componentDir, './04-templates'),
-        'ct-base': resolve(coreComponentDir, './00-base'),
-        'ct-atoms': resolve(coreComponentDir, './01-atoms'),
-        'ct-molecules': resolve(coreComponentDir, './02-molecules'),
-        'ct-organisms': resolve(coreComponentDir, './03-organisms'),
-        'ct-templates': resolve(coreComponentDir, './04-templates'),
         civictheme: componentDir,
-        [parse(import.meta.dirname).name]: componentDir
       },
     }),
     sdcPlugin({ path: componentDir }),
