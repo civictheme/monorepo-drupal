e7c99d7e64c7a3b3aac843c8fddc51ab38537373
Joshua Fernandes <joshua.1234511@yahoo.in>
2026-03-31 10:18:28 +0530

    [3517035] Replace jQuery with vanilla JS in error focus behavior.

--- web/themes/contrib/civictheme/assets/js/status-messages/status-messages.js
+++ web/themes/contrib/civictheme/assets/js/status-messages/status-messages.js
@@ -1,16 +1,19 @@
 // CivicTheme status-messages.js
-(function ($, Drupal) {
+(function (Drupal) {
   Drupal.behaviors.ctFocusErrorSummary = {
-    attach: function (context, settings) {
-      // Wait for DOM update after AJAX or normal load
+    attach: function () {
+      // Use setTimeout to ensure the error element is rendered in the DOM.
       setTimeout(function () {
-        // Find any element with id starting with 'error-summary-'
-        var $error = $('[id^="error-summary-"]', context).first();
-        if ($error.length) {
-          $error.attr('tabindex', '-1'); // ensure focusable (in case)
-          $error.focus();
+        // Always search from document since error messages may be outside
+        // the current behavior context.
+        var error = document.querySelector('[id^="error-summary-"]');
+        if (error && !error.hasAttribute('data-ct-error-focused')) {
+          error.setAttribute('data-ct-error-focused', 'true');
+          error.setAttribute('tabindex', '-1');
+          error.focus();
+          error.scrollIntoView({ behavior: 'smooth', block: 'center' });
         }
       }, 30);
-    }
+    },
   };
-})(jQuery, Drupal);
+})(Drupal);
